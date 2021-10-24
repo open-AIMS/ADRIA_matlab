d080ade51dbe1d6212658c57120fb82f793a2611
ConnectedSystems <takuyai@gmail.com>
2021-10-25 09:51:40 +1100

    fix incorrect file being loaded for comparison

--- tests/resultMatchTest.m
+++ tests/resultMatchTest.m
@@ -1,6 +1,6 @@
 if exist(['../Outputs', '/Results60'], 'file')
     expected = load("../Outputs/Orig_Results_RCP60_Alg1_reef");
-    changed = load("../Outputs/Results_RCP60_Alg1_ES");
+    changed = load("../Outputs/Results_RCP60_Alg1_reef");
     
     assert(isequaln(expected, changed));
 end
