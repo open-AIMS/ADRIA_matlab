865d996813efe842e5657e1fb2c2e343e8235fc8
ConnectedSystems <takuyai@gmail.com>
2021-10-27 12:15:11 +1100

    Add docstring for DHW sequence generation

--- ADRIAfunctions/ADRIA_DHWprojectfun.m
+++ ADRIAfunctions/ADRIA_DHWprojectfun.m
@@ -1,8 +1,23 @@
 function DHWdisttime = ADRIA_DHWprojectfun(tf,nsites,mdhwdist0,sdhwdist0,dhwmax25,RCP,wb1,wb2)
+% Generates time sequence of Degree Heating Weeks for all sites
+%
+% Inputs: 
+%     tf        : int, number of time steps to generate
+%     nsites    : int, number of sites to generate time sequence for
+%     mdhwdist0 : float, mean of DHW distribution
+%     sdhwdist0 : float, standard deviation of DHW distribution
+%     dhwmax25  : float, Maximum DHW at year 2025
+%     RCP       : int, RCP scenario
+%     wb1       : float, scale `a` for weibull distribution
+%     wb2       : float, shape `b` for weibull distribution
+%
+% Outputs:
+%     DHWdisttime : 
+%         2D table of (timestep, nsites) indicating DHWs for each site
 
-%explanation
+% RCP heatrates
 if RCP == 26
-    heatrate = 0.2; 
+    heatrate = 0.2;
 elseif RCP == 45
     heatrate = 0.3;
 elseif RCP == 60
@@ -15,10 +30,11 @@ end
 
 DHW = ones(tf,nsites); %initialise
 for t = 1:tf
-    wblsample = wblrnd(wb1,wb2,1); %sample from under the maxDHW curve
-    wblsample(wblsample>1) = 1; 
-    DHW(t,:) = wblsample*((dhwmax25 + normrnd(mdhwdist0,sdhwdist0))+heatrate*t); 
+    wblsample = wblrnd(wb1,wb2,1); % sample from under the maxDHW curve using the weibull distribution
+    wblsample(wblsample>1) = 1;
+    DHW(t,:) = wblsample*((dhwmax25 + normrnd(mdhwdist0,sdhwdist0))+heatrate*t);
 end
+
 DHW(DHW<0) = 0; 
 DHWdisttime = DHW;
 
