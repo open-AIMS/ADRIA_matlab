759d2030627f8a801c796f8fc00cca952693f98b
Rosejoycrocker <56939532+Rosejoycrocker@users.noreply.github.com>
2021-11-01 08:26:40 +1030

    Some small changes to try and make the optimisation more efficient
    - changing how CrtWts is fed to objective to it is not recalculated
    - adding more inputs to objective so optimisation focuses only on intervention parameters

--- examples/ADRIAOptimisation.m
+++ examples/ADRIAOptimisation.m
@@ -3,10 +3,11 @@ b = 3; % PrSites
 c = 60; % RCP
 d = 1; % out_ind
 
+CrtWts = CriteriaWeights();  
 % initialise parameters
 % x0 = [Guided,Seed1,Seed2,SRM,Aadpt,Natad]
 x0 = [1 0 0 0 0 0 ];
-ObjectiveFunction = @(x) -1*ObjectiveFunc(x,a,b,c,d);
+ObjectiveFunction = @(x) -1*ObjectiveFunc(x,a,b,c,d,CrtWts);
 
 % upper bounds on x
 ub = [0.001,0.001,5,5,0.1];
