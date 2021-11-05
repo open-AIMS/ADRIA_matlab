9221965d017933b665847ca4d10b2c7c59a73307
ConnectedSystems <takuyai@gmail.com>
2021-11-05 13:43:42 +1100

    monte carlo scenario generation example

--- examples/mc_scenario_generation.m
+++ examples/mc_scenario_generation.m
@@ -1,13 +1,17 @@
 inter_opts = interventionDetails();
+criteria_opts = criteriaDetails();
+
+
+combined_opts = [inter_opts; criteria_opts];
 
 % Generate using simple monte carlo
 % Create selection table based on lower/upper parameter bounds
 N = 20;
 p_sel = table;
-for p = 1:height(inter_opts)
-    curr_row = inter_opts.option_bounds{p};
+for p = 1:height(combined_opts)
+    curr_row = combined_opts.option_bounds{p};
     
-    if inter_opts.ptype{p} ~= "float"
+    if combined_opts.ptype{p} ~= "float"
         selection = randi(curr_row, N, 1);
     else
         a = curr_row(1);
@@ -16,11 +20,11 @@ for p = 1:height(inter_opts)
         % p_sel.(inter_opts.name{p}) = selection;
     end
     
-    p_sel.(inter_opts.name{p}) = selection;
+    p_sel.(combined_opts.name{p}) = selection;
 end
 
 
-converted_tbl = convertScenarioSelection(p_sel, inter_opts);
+converted_tbl = convertScenarioSelection(p_sel, combined_opts);
 
 % table2array(converted_tbl)
 converted_tbl
