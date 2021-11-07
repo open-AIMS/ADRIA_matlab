0bc3f30a8708c3afb5f88547735b8acbc0f797e7
KRNA01 <91235035+KRNA01@users.noreply.github.com>
2021-11-08 08:49:52 +1000

    updates to reefmod translations

--- ecosystemservices/reefcondition_from_reefmod.m
+++ ecosystemservices/reefcondition_from_reefmod.m
@@ -3,7 +3,7 @@
 %% Settings
 criteria_thr = 0.75;  %threshold for how many criteria need to be met in order for a condition category to be satisfied. 
 COTS_outbreak_threshold = 0.2; 
-metrics = [1,3,4,5,6,7,8];  % see below for metrics implemented
+metrics = 2; %[1,2,3,4,5,6,7,8];  % see below for metrics implemented
 
 %% Structure of the ReefConditionIndex when completed here ('comp' means complementary, i.e. 1-metric)
 %               totcov: [448×85 single]
