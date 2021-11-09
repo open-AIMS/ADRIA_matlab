ed2a4fa600d067026cf321c437896ae34d5ea084
ConnectedSystems <takuyai@gmail.com>
2021-11-09 19:18:47 +1100

    Expanded docstrings

---
+++ ADRIAfunctions/SystemInfo/estimateRuntime.m
@@ -0,0 +1,28 @@
+function est = estimateRuntime(n_sims)
+% Estimate total runtime based on a separate early indicative trial.
+%
+% NOTE: 
+%     Assumes all detected cores are used.
+%     These are indicative estimates only with no guarantee of accuracy
+%     or reliability.
+%
+% Inputs:
+%     n_sims : int, number of simulations to be run
+%
+% Outputs:
+%     est : float, estimated runtime (in seconds)
+
+    % Estimate from original run set
+    trial_runtime = 6.9311;  % trial run time
+    trial_CPU_freq = 3800;     % boost frequency
+    
+    this_comp = cpuinfo();
+    tmp = split(this_comp.Clock, " ");
+    tmp = tmp(1);
+    this_CPU_freq = str2double(tmp{1});
+    avail_cores = this_comp.TotalCores;
+    
+    est_run = (trial_CPU_freq / this_CPU_freq) * trial_runtime;
+    est = max(est_run, ((n_sims * est_run) / avail_cores));
+    est = round(est, 2);
+end
