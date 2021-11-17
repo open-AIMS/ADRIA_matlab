8200069d6437b2ee86a08728e90c31cc4d21d917
Rosejoycrocker <56939532+Rosejoycrocker@users.noreply.github.com>
2021-11-18 10:16:32 +1030

    Improvements to optimisation functions
    - now allows option to chose which metrics to optimise over (from TC,S,E,CES,PES)
    - if one metric chosen, uses a single obj simulated annealing algorithm to optimise
    - if two or more metrics chosen , uses a multi-obj ga algorithm to find intervention pareto set
    - options to retrieve shell variables as input for hpc run compatibility
    - ADRIAOptimisation is now a function, so an example script optimisation_example.m has also been added for function usage
    - optimisation_example.m gives examples of optimising for just one metric av_TC and for the two metrics av_TC and av_CES.
    - health warning: still takes a long time to run, currently thinking of how to implement shortcuts in runADRIA so that not all outputs are calculated if not requested (e.g. if only TC requested, runADRIA does not calculate S and E)

--- examples/ADRIAOptimisation.m
+++ examples/ADRIAOptimisation.m
@@ -1,66 +1,89 @@
-% ADRIAOptimisation collects shell environment variables for RCP and
-% PrSites and runs an optimisation algorithm to find average total
-% coral cover maximising values of the interventions
-
-alg = 1; % alg_ind
-% indicate which variables to optimise over out of [TC,E,S,CES,PES]
-% 1 : include, 0 : don't include
-% sum(opti_ind) >=1
-opti_ind = [1 0 0 0 0]; 
-
-% get shell variables
-prsites = str2num(getenv('PrSites')); % PrSites
-rcp = str2num(getenv('RCP'));; % RCP
-
-% use default criteria weights
-CrtWts = CriteriaWeights();  
-%load parameter file
-[params, ecol_parms] = ADRIAparms(); % environmental and ecological parameter values etc
-
-% initialise parameters
-% x0 = [Seed1,Seed2,SRM,Aadpt,Natad]
-x0 = [0 0 0 0 0];
-
-% objective function for simulated annealing function is negative (as
-% solves the minimisation) and must have a single vector input and scalar
-% output
-if sum(opti_ind) == 1
-    % if opti_ind = 1, perform a single objective optimisation for average
-    % TC
-    ObjectiveFunction = @(x) -1*ObjectiveFunc(x,alg,opti_ind,prsites,rcp,CrtWts,params,eco_parms);
+function [x,fval] = ADRIAOptimisation(alg,opti_ind,varargin)
+    % ADRIAOptimisation takes variables for RCP and
+    % PrSites and runs an optimisation algorithm to maximise outputs with
+    % respect to the intervention variables Seed1, Seed2, SRM, AsAdt,
+    % NatAdt
+    % Inputs :
+    %        if 2 inputs, will use shell variables for PrSites and RCP
+    %        if > 2 inputs, will use these as PrSites and RCP
+    %        Input order:
+    %        alg : indicates MCDA algorithm to be used 
+    %              1 - Order Ranking
+    %              2 - TOPSIS
+    %              3 - VIKOR
+    %        opti_ind : indicates which outputs to optimise over out of [TC,E,S,CES,PES]
+    %              1 - include, 0 - don't include, sum(opti_ind) >=1
+    %              ex., opti_ind = [1 0 0 0 0] optimises for average TC only
+    %        varargin{1} : prsites (1,2,3)
+    %        varargin{2} : rcp (rcp scenario value 2.6,4.5,6.0,8.5)
+    %        varargin{3} : optional appendage to filename (e.g. run no. etc)
+    % Outputs :   
+    %         x : [Seed1,Seed2,SRM,Aadpt,Natad] which maximise the chosen
+    %             ADRIA output metrics (will represent a pareto front if
+    %             multiple values are chosen to optimise over.
+    %         fval : the max value/optimal value of the chosen metrics 
     
-    % upper bounds on x
-    ub = [0.001,0.001,5,5,0.1];
-    % lower bounds on x
-    lb = [0,0,0,0,0];
+    if nargin == 2
+        % get shell variables
+        prsites = str2double(getenv('PrSites'));
+        rcp = str2double(getenv('RCP'));; 
+    elseif nargin>2
+        prsites = varargin{1};
+        rcp = varargin{2};
+    end
+    % use default criteria weights
+    CrtWts = CriteriaWeights();  
+    % load parameter file
+    [params, ecol_parms] = ADRIAparms(); % environmental and ecological parameter values etc
 
-    % begin optimisation algorithm
-    x = simulannealbnd(ObjectiveFunction,x0,lb,ub);
-    
-else
-    % if sum(opti_ind)>1  perform a multi-objective optimisation over the
-    % specified outputs
-    ObjectiveFunction = @(x) -1*ObjectiveFunc(x,alg,prsites,rcp,opti_ind,CrtWts,params,eco_parms);
-    
-    % upper bounds on x
-    ub = [0.001,0.001,5,5,0.1];
-    % lower bounds on x
-    lb = [0,0,0,0,0];
-    
-    % no constraint equations for now
-    A = [];
-    b = [];
-    Aeq = [];
-    beq = [];
-    
-    % no. of variables to optimise for = no. of interventions
-    nvar = 5;
-    
-    % begin optimisation algorithm
-    x = gamultiobj(ObjectiveFunction,x0,A,b,Aeq,beq,lb,ub);
+    % initialise parameters (could later randomise this)
+    % x0 = [Seed1,Seed2,SRM,Aadpt,Natad]
+    x0 = [0 0 0 0 0];
+
+    if sum(opti_ind) == 1
+        % if opti_ind = 1, perform a single objective optimisation for
+        % chosen output (obj is negative as default is the minimisation)
+        ObjectiveFunction = @(x) -1*ObjectiveFunc(x,CrtWts,params,ecol_parms,alg,prsites,rcp,opti_ind);
+
+        % upper bounds on x
+        ub = [0.001,0.001,6,6,0.1];
+        % lower bounds on x
+        lb = [0,0,0,0,0];
+
+        % begin optimisation algorithm
+        [x, fval] = simulannealbnd(ObjectiveFunction,x0,lb,ub);
+        fval = -1*fval;
+
+    else
+        % if sum(opti_ind)>1  perform a multi-objective optimisation over the
+        % specified outputs
+        ObjectiveFunction = @(x) -1*ObjectiveFunc(x,CrtWts,params,ecol_parms,alg,prsites,rcp,opti_ind);
+
+        % upper bounds on x
+        ub = [0.001,0.001,6,6,0.1];
+        % lower bounds on x
+        lb = [0,0,0,0,0];
+
+        % no constraint equations for now
+        A = [];
+        b = [];
+        Aeq = [];
+        beq = [];
+
+        % no. of variables to optimise for = no. of interventions
+        nvar = 5;
+
+        % begin optimisation algorithm
+        [x, fval] = gamultiobj(ObjectiveFunction,nvar,A,b,Aeq,beq,lb,ub);
+        fval = -1*fval;
+    end
+    % label file with key parameters
+    filename = sprintf('ADRIA_opt_out_RCP%2.0f_PrSites%1.0d_Alg%1.0d.csv',rcp,prsites,alg);
+    % add filename appendage if specified
+    if nargin == 5
+        filename = strcat(varargin{4},filename);
+    end
+    % save as structure
+    s = struct('interv',x,'output',fval);
+    save(filename,'s')
 end
-
-% label file with key parameters
-filename = sprintf('ADRIA_opt_out_RCP%2.0f_PrSites%1.0d_Alg%1.0d.csv',rcp,prsites,alg);
-% save as csv
-savematrix(x,filename)
---
+++ examples/optimisation_example.m
@@ -0,0 +1,44 @@
+%% Example for 2 simple usages of the optimisation function ADRIAOptimisation
+%% 1 : only optimise for average total coral cover av_TC
+
+% use simplest MDCA algorithm for now
+alg = 1;
+
+% use all sites (C)
+prsites = 3; 
+
+% optimisation specification - want to optimise over TC only
+opti_ind = [1,0,0,0,0];
+
+% declare filename appendage to tag as example
+file_ap = 'Example1obj';
+
+% perform optimisation (takes a while, be warned, improvements to
+% efficiency to be made)
+[x,fval] = ADRIAOptimisation(alg,opti_ind,prsites,alg,file_ap);
+
+% print results (also automatically saved to a struct in a .mat file) 
+sprintf('Optimal intervention values were found to be Seed1: %1.4f, Seed2: %1.4f, SRM: %2.0f, AsAdt: %2.0f, NatAdt: %1.2f, with av_TC = %1.4f',...
+    x(1),x(2),x(3),x(4),x(5),fval);
+
+%% 2 : optimise for average total coral cover av_TC and scope for cultural ecosystem services av_CES
+
+% use simplest MDCA algorithm for now
+alg = 1;
+
+% use all sites (C)
+prsites = 3; 
+
+% optimisation specification - want to optimise over TC and CES now
+opti_ind = [1,0,0,1,0];
+
+% declare filename appendage to tag as example
+file_ap = 'Example2obj';
+
+% perform optimisation (takes a while, be warned, improvements to
+% efficiency to be made)
+[x,fval] = ADRIAOptimisation(alg,opti_ind,prsites,alg,file_ap);
+
+% print results (also automatically saved to a struct in a .mat file) 
+sprintf('Optimal intervention values were found to be Seed1: %1.4f, Seed2: %1.4f, SRM: %2.0f, AsAdt: %2.0f, NatAdt: %1.2f, with av_TC = %1.4f, av_CES = %1.4f',...
+    x(1),x(2),x(3),x(4),x(5),fval(1),fval(2));
