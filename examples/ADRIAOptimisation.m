9de0168d2d075aba94f8eaab36196738484c32c5
ConnectedSystems <takuyai@gmail.com>
2021-11-21 16:55:26 +1100

    Update example script to use `*Details()` group of functions
    Note that `out_name` is used in favour of `out_ind` to avoid modifying runADRIA (which would otherwise stops its use for other examples)

--- examples/ADRIAOptimisation.m
+++ examples/ADRIAOptimisation.m
@@ -2,35 +2,48 @@
 % PrSites and runs a simulated annealing algorithm to find avergae totl
 % coral cover maximising values of the interventions
 
-alg = 1; % alg_ind
+alg = 1;  % Use Order Ranking
 
 % get shell variables
 prsites = str2num(getenv('PrSites')); % PrSites
-rcp = str2num(getenv('RCP'));; % RCP
+rcp = str2num(getenv('RCP')); % RCP
+out_name = 'TC'; % out_ind
 
-numvar = 1; % out_ind
+% Use default criteria weights
+criteria_opts = criteriaDetails();
+CrtWts = cell2mat(criteria_opts.defaults)';
 
-% use default criteria weights
-CrtWts = CriteriaWeights();  
+% Perturb subset of intervention options
+i_params = interventionDetails();
 
-% initialise parameters
-% x0 = [Seed1,Seed2,SRM,Aadpt,Natad]
-x0 = [0 0 0 0 0];
+% Filter to target interventions
+p_names = i_params.name;
+rules = false;
+for target = {'Seed1', 'Seed2', 'SRM', 'Aadpt', 'Natad'}
+    rules = rules | (p_names == target);
+end
+
+subset = i_params(rules, :);
+
+% Initialise parameters
+x0 = cell2mat(subset.defaults);
+
+% Upper/Lower bounds of x
+lb = cell2mat(subset.lower_bound);
+ub = cell2mat(subset.upper_bound);
+
+[params, ecol_parms] = ADRIAparms();
 
 % objective function for simulated annealing function is negative (as
 % solves the minimisation) and must have a single vector input and scalar
 % output
-ObjectiveFunction = @(x) -1*ObjectiveFunc(x,alg,prsites,rcp,numvar,CrtWts);
-
-% upper bounds on x
-ub = [0.001,0.001,5,5,0.1];
-% lower bounds on x
-lb = [0,0,0,0,0];
+ObjectiveFunction = @(x) -1*ObjectiveFunc(x,alg,prsites,rcp,out_name,CrtWts, params, ecol_parms);
 
 % begin optimisation algorithm
 x = simulannealbnd(ObjectiveFunction,x0,lb,ub);
 
 % label file with key parameters
 filename = sprintf('ADRIA_opt_out_RCP%2.0f_PrSites%1.0d_Alg%1.0d.csv',rcp,prsites,alg);
-% save as csv
-savematrix(x,filename)
+
+% Save as CSV
+saveData(x, filename, 'csv')
