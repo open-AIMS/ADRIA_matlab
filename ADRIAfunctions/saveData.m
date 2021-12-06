c21bc924735a5277cbb049e27401a54a457c8c46
ConnectedSystems <takuyai@gmail.com>
2021-12-06 14:26:28 +1100

    Add support for saving struct to netcdf

--- ADRIAfunctions/saveData.m
+++ ADRIAfunctions/saveData.m
@@ -9,6 +9,11 @@ function saveData(data, filename, dim_spec, nc_varname)
     % support overwriting existing variables and so variable names must be
     % unique.
     %
+    % Note: If NetCDF is specified and `data` is a struct,
+    %    this function will attempt to create an entry for each item
+    %    and infer its dimensions (up to 5) and name (using the struct
+    %    fieldnames).
+    %
     % Inputs:
     %     data       : any, data to save
     %     filename   : str (optional), file name and location of file.
@@ -29,6 +34,11 @@ function saveData(data, filename, dim_spec, nc_varname)
     %
     %     % Saving a 5x5 dimension array to NetCDF
     %     saveData(data, 'example.nc', {'x', 5, 'y', 5}, 'varname')
+    %
+    %     % Saving a struct
+    %     % Supports up to 5 dimensions per variable
+    %     tmp = struct('x', rand(2,2), 'y', rand(2,2,2,2))
+    %     saveData(tmp, 'example.nc')
     valid_formats = {'mat', 'csv', 'nc'};
 
     if exist('filename', 'var')
@@ -68,16 +78,33 @@ function saveData(data, filename, dim_spec, nc_varname)
     elseif strcmpi(fmt, 'csv')
         writematrix(data, filename);
     elseif strcmpi(fmt, 'nc')
-        if ~exist('dim_spec', 'var')
-            error('No data dimension details provided.');
+        if ~isstruct(data)
+            if ~exist('dim_spec', 'var')
+                error('No data dimension details provided.');
+            end
+
+            if ~exist('nc_varname', 'var')
+                nc_varname = 'data';
+            end
+            
+            nccreate(filename, nc_varname, 'Dimensions', dim_spec);
+            ncwrite(filename, nc_varname, data);
+        else
+            f_names = fieldnames(data);
+            n_vars = length(f_names);
+            for i = 1:n_vars
+                tmp_fn = f_names{i};
+                t_data = data.(tmp_fn);
+                [x, y, z, v, w] = size(t_data);
+
+                nccreate(filename, tmp_fn, 'Dimensions', ...
+                    {[tmp_fn '_x'], x, [tmp_fn '_y'], y, ...
+                     [tmp_fn '_z'], z, [tmp_fn '_v'], v, [tmp_fn '_w'], w})
+                ncwrite(filename, tmp_fn, t_data)
+            end
         end
 
-        if ~exist('nc_varname', 'var')
-            nc_varname = 'data';
-        end
-
-        nccreate(filename, nc_varname, 'Dimensions', dim_spec);
-        ncwrite(filename, nc_varname, data);
+        
     else
         error(file_msg)
     end
