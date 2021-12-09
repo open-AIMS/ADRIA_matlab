7f37f486f4667145c96d445847e8ad3148ccb269
Rosejoycrocker <56939532+Rosejoycrocker@users.noreply.github.com>
2021-12-09 10:52:15 +1030

    Added some histogram plotting and example inferences with new data set
    - new inference examples show: a) comparing an inference for 2 algorithms and b) comparing 4-yearly histograms for 2 RCPs
    - moved example script to correct folder
    - some other file organisation

--- ADRIAfunctions/OutputConversionFunctions/test_file_creation_w_sites.m
+++ ADRIAfunctions/OutputConversionFunctions/test_file_creation_w_sites.m
@@ -22,22 +22,37 @@ Data = BBNTableMCData(filetype,rcps,algs,col_names,nsims,yr,sites,metrics);
 head(Data)
 
 % declare nodes in BBN
-Names = {'RCP'; 'Alg';'Years';'Guided'; 'PrSites';'Seed1'; 'Seed2'; 'SRM'; 'AssAdt'; 'NatAdt'; 'Seedyrs';'Shadeyrs';...
+Names = {'RCP'; 'Alg';'Years';'Sites';'Guided'; 'PrSites';'Seed1'; 'Seed2'; 'SRM'; 'AssAdt'; 'NatAdt'; 'Seedyrs';'Shadeyrs';...
     'CC';'S';'E'};
 
 
 % construct ParentCell, a cell structure of size 1*(no. of nodes)
 % each cell contains the parents of the node corresponding the the cell no.
-ParentCell = cell(1,15);
-for i = 1:12
+ParentCell = cell(1,16);
+for i = 1:13
     ParentCell{i} = [];
 end
-ParentCell{13} = 1:12;
-ParentCell{14} = 1:12;
-ParentCell{15} = 1:12;
+ParentCell{14} = 1:13;
+ParentCell{15} = 1:13;
+ParentCell{16} = 1:13;
 
 Data = table2array(Data);
 
 R = bn_rankcorr(ParentCell, Data, 1, 0, Names);
 
 bn_visualize(ParentCell,R,Names,gca);
+
+% example - what is the mean coral cover, E and S on site 26 for an RCP of 4.5,
+% at year 10, with guided interventions, using all sites (Prsites = 3 ) and
+% only seed1 and seed2 = 0.0005,seedyrs 12 and shadeyrs 3
+
+% note that when performing inferences in this larger network, not enough
+% degrees of freedom (e.g. setting too many variables as known) can result
+% in Nans. It seems at least 5 variables need to be unknown to allow
+% calculation.
+
+% nodes we know
+inf_cells = 1:13;
+% their values
+vals = [26,3,10,26,1,3,0.0009,0.0009,0,0,0,12,3];
+outcome1 = inference(inf_cells,vals,R,Data,'mean',1000,'near');
