636941e2bf805737d4c77aef89d68e229c411d31
Rosejoycrocker <56939532+Rosejoycrocker@users.noreply.github.com>
2021-12-09 12:17:34 +1030

    Adding some plotting tests (in progress)

--- ADRIAfunctions/ADRIA_saveResults.m
+++ ADRIAfunctions/ADRIA_saveResults.m
@@ -1,26 +1,32 @@
-function ADRIA_saveResults(data, filename)
-    % Save results to file.
+function ADRIA_saveResults(data, filename, dim_spec, nc_varname)
+    % Save data to file in CSV, `.mat` or NetCDF format.
     %
     % If file is not specified, generates a filename based on date/time
-    % 
+    %
     % Inputs:
-    %     data     : any, data to save
-    %     filename : str, file name and location to save data to
-    if ~exist('filename', 'var')
-        
-        % TODO : Remove explicit "Outputs" path
-        right_now = datetime(now, 'ConvertFrom', 'datenum');
-        right_now = replace(string(right_now), ' ', '_');
-        right_now = replace(right_now, ':', '');
-        
-        filename = strcat('ADRIA_results_', right_now, '.mat');
-        
-        fprintf('File name not provided. Saving results to %s \n', filename)
+    %     data       : any, data to save
+    %     filename   : str (optional), file name and location of file.
+    %                    Defaults to `ADRIA_results_[current time].csv`
+    %                    if nothing specified.
+    %     dim_spec   : cell array (optional), name/dimensions of `data`.
+    %                    Required for 'nc'.
+    %                    e.g., `{'x_name', x_data, 'y_name', y_data}`
+    %     nc_varname : str (optional), variable name to save data to in the
+    %                    NetCDF file. Required for 'nc'.
+    %
+    % Example:
+    %     data = rand(5,5)
+    %
+    %     % These are equivalent
+    %     saveData(data, 'example')
+    %     saveData(data, 'example.csv')
+    %
+    %     % Saving a 5x5 dimension array to NetCDF
+    %     saveData(data, 'example.nc', {'x', 5, 'y', 5}, 'varname')
+    warning("ADRIA_saveResults() is deprecated. Use saveData() instead.")
+    if nargin < 3
+        saveData(data, filename)
+    else
+        saveData(data, filename, dim_spec, nc_varname)
     end
-    
-    if ~endsWith(filename, '.mat')
-        filename = strcat(filename, '.mat');
-    end
-
-    save(filename, 'data');    
 end
---
+++ examples/test_nc_file_creation.m
@@ -0,0 +1,6 @@
+load('Outputs/Results_RCP26_Alg1.mat')
+
+
+fileloc = 'Outputs/MCDA_example.nc';
+
+%Data = ncread(fileloc,'');
