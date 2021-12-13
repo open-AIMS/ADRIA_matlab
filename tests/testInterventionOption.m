bbc011c8aba9c4771e0a59ed8d66f4c846c9efea
ConnectedSystems <takuyai@gmail.com>
2021-12-13 14:42:30 +1100

    Update table option tests

--- tests/testInterventionOption.m
+++ tests/testInterventionOption.m
@@ -1,16 +1,15 @@
-default_interventions = interventionSpecification();
+def_interv = interventionDetails();
 
-% Test returned type is of struct
-assert(isequal(class(default_interventions), 'struct'))
+% Test returned type is of table
+assert(isequal(class(def_interv), 'table'))
 
-% Test struct gets returned with default values set
-assert(all(default_interventions.Guided == [0, 1]))
-assert(isequal(default_interventions.PrSites, 3))
-assert(all(default_interventions.Seed1 == [0, 0.0005, 0.0010]))
-assert(isequal(default_interventions.Seed2, 0))
-assert(isequal(default_interventions.SRM, 0))
-assert(all(default_interventions.Aadpt == [6, 12]))
-assert(isequal(default_interventions.Natad, 0.05))
-assert(isequal(default_interventions.Seedyrs, 10))
-assert(isequal(default_interventions.Shadeyrs, 1))
-assert(isequal(default_interventions.sims, 50))
+% Test table gets returned with default bounds set
+assert(all(def_interv{1, "raw_bounds"}{1} == [0 1]), "Guided bounds incorrect!")
+assert(all(def_interv{2, "raw_bounds"}{1} == [1 3]), "PrSites bounds incorrect!")
+assert(all(def_interv{3, "raw_bounds"}{1} == [0 1.0000e-03]), "Seed1 bounds incorrect!")
+assert(all(def_interv{4, "raw_bounds"}{1} == [0 1]), "Seed2 bounds incorrect!")
+assert(all(def_interv{5, "raw_bounds"}{1} == [0 1]), "SRM bounds incorrect!")
+assert(all(def_interv{6, "raw_bounds"}{1} == [6 12]), "Aadpt bounds incorrect!")
+assert(all(def_interv{7, "raw_bounds"}{1} == [0.0100 0.1000]), "Natad bounds incorrect!")
+assert(all(def_interv{8, "raw_bounds"}{1} == [10 15]), "Seedyrs bounds incorrect!")
+assert(all(def_interv{9, "raw_bounds"}{1} == [1 5]), "Shadeyrs bounds incorrect!")
