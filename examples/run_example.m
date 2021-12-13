002bb5ad075c273b6c965e1d9278edef0e68f21e
ConnectedSystems <takuyai@gmail.com>
2021-12-13 14:00:26 +1100

    Update core example files

--- examples/run_example.m
+++ examples/run_example.m
@@ -1,97 +1,134 @@
-% MARKED FOR DEPRECATION
-
-% set random number seed to ensure consistent results for test
+% Example script illustrating running ADRIA scenarios
 rng(101)
 
-%% Set up ADRIA project
-% Specify path to ADRIA project. Opens current directory if nothing is
-% provided
-ADRIAsetup()
+%% Generate monte carlo samples
 
-%% Generate parameter combinations
-% Create struct with default intervention values
-% Options and default values:
-%   Guided = [0, 1];  0 or 1 for guided or unguided intervention 
-%                     (can specify both as an array)
-%   PrSites = 3;      1, 2 or 3 for selection of site to intervene on
-%   Seed1 = [0, 0.0005, 0.0010];  Seeding scenarios for 2 coral types
-%   Seed2 = 0;
-%   SRM = 0;          Shading levels
-%   Aadpt = [6, 12];  Assisted adaptation levels
-%   Natad = 0.05;     Natural adaptation levels
-%   Seedyrs = 10;     how many years to seed, starting in 2026
-%   Shadeyrs = 1;     how many years to shade, starting in 2026
-%   sims = 50;        how many simulations to run
-%   RCP = 60;
+% Number of scenarios
+N = 8;
+num_reps = 3;  % Number of replicate RCP scenarios
 
-% Specify options above by name to change settings
-N = 10;
+% Collect details of available parameters
+inter_opts = interventionDetails();
+criteria_opts = criteriaDetails();
 
-interventions = interventionSpecification(sims=N);
+% Create main table listing all available parameter options
+combined_opts = [inter_opts; criteria_opts];
 
-% Specific settings can be changed as with interventionSpecification()
-criteria_weights = criteriaWeights();
+% Generate samples using simple monte carlo
+% Create selection table based on lower/upper parameter bounds
+p_sel = table;
+for p = 1:height(combined_opts)
+    a = combined_opts.lower_bound{p};
+    b = combined_opts.upper_bound{p};
+    
+    selection = (b - a).*rand(N, 1) + a;
+    
+    p_sel.(combined_opts.name{p}) = selection;
+end
 
+%% Parameter prep
+
+% Creating dummy permutations for core ADRIA parameters
+% (environmental and ecological parameter values etc)
+% This process will be replaced
+[params, ecol_params] = ADRIAparms();
+param_tbl = struct2table(params);
+ecol_tbl = struct2table(ecol_params);
+
+param_tbl = repmat(param_tbl, N, 1);
+ecol_tbl = repmat(ecol_tbl, N, 1);
+
+% Convert sampled values to ADRIA usable values
+% Necessary as samplers expect real-valued parameters (e.g., floats)
+% where as in practice ADRIA makes use of integer and categorical
+% parameters
+converted_tbl = convertScenarioSelection(p_sel, combined_opts);
+
+% Separate parameters into components
+% (to be replaced with a better way of separating these...)
+interv_scens = converted_tbl(:, 1:9);  % intervention scenarios
+criteria_weights = converted_tbl(:, 10:end);
+
+% use order-ranking for example
 alg_ind = 1;
 
-%% Future approach (implementation above will be deprecated)
-% inter_opts = interventionDetails();
-% criteria_opts = criteriaDetails();
+%% Load site specific data
+[F0, xx, yy, nsites] = ADRIA_siteTable('MooreSites.xlsx');
+[TP_data, site_ranks, strongpred] = ADRIA_TP('MooreTPmean.xlsx', params.con_cutoff);
 
-% combine parameters
-% combined_opts = [inter_opts; criteria_opts];
+%% setup for the geographical setting including environmental input layers
+% Load wave/DHW scenario data
+% Generated with generateWaveDHWs.m
+% TODO: Replace these with wave/DHW projection scenarios instead
+fn = strcat("Inputs/example_wave_DHWs_RCP", num2str(params.RCP), ".nc");
+wave_scens = ncread(fn, "wave");
+dhw_scens = ncread(fn, "DHW");
 
-% p_sel = table;
-% converted_tbl = convertScenarioSelection(p_sel, combined_opts);
-% converted_tbl
+%% Scenario runs
+% Currently running over unique interventions and criteria weights only for
+% a limited number of RCP scenarios.
+%
+% In actuality, this would be done for some combination of:
+% intervention * criteria * environment parameters * ecological parameter
+%     * wave_scen * dhw_scen * alg_ind * N_sims
+% where the unique combinations would be generated via some quasi-monte 
+% carlo sequence, or through some user-informed process.
 
-% Set default criteria weighting
-% wave_stress = 1  % wave stress avoidance
-% heat_stress = 1  % heat stress avoidance
-% shade_connectivity = 0  % Connectivity when shading/cooling
-% seed_connectivity = 0   % Connectivity when seeding
-% coral_cover_high = 0    % High coral cover intervention
-% coral_cover_low = 0     % Low coral cover intervention
-% seed_priority = 1       % Seed at strongest sources for priority sites
-% shade_priority = 0      % Shade at strongest sources for priority sites
-% deployed_coral_risk_tol = 1  % Risk Tolerance wrt Deployed Corals
-
-
-% Algorithm choice
-%  1 = OrderRanking
-%  2 = TOPSIS
-%  3 = VIKOR 
-%  4 = Multi-Obj GA 
-
-%% LOAD parameter file
-[params, ecol_parms] = ADRIAparms(); % environmental and ecological parameter values etc
-
-%% Run ADRIA
+% Select random subset of RCP conditions WITHOUT replacement
+n_rep_scens = length(wave_scens);
+rcp_scens = datasample(1:n_rep_scens, num_reps, 'Replace', false);
+w_scens = wave_scens(:, :, rcp_scens);
+d_scens = dhw_scens(:, :, rcp_scens);
 
 tic
-reef_condition_metrics = runADRIA(interventions, criteria_weights, params, ecol_parms, alg_ind);
+Y = runScenarios(interv_scens, criteria_weights, param_tbl, ecol_tbl, ...
+                 TP_data, site_ranks, strongpred, num_reps, ...
+                 w_scens, d_scens, alg_ind);
 tmp = toc;
 
-disp(strcat("Took ", num2str(tmp), " seconds to run ", num2str(N), " sims (", num2str(tmp/N), " seconds per run)"))
+disp(strcat("Took ", num2str(tmp), " seconds to run ", num2str(N*num_reps), " simulations (", num2str(tmp/(N*num_reps)), " seconds per run)"))
 
-%% Analysis Example
+%% post-processing
+% collate data across all scenario runs
 
-% Convert reef results to ecosystem service metrics
-ecosys_results = Corals_to_Ecosys_Services(reef_condition_metrics);
+% tf = params.tf;
+% nspecies = 4;
+% processed = struct('TC', zeros(tf, nsites, N, num_reps), ...
+%                    'C', zeros(tf, nspecies, nsites, N, num_reps), ...
+%                    'E', zeros(tf, nsites, N, num_reps), ...
+%                    'S', zeros(tf, nsites, N, num_reps));
+% for i = 1:N
+%     for j = 1:num_reps
+%         processed.TC(:, :, i, j) = Y.TC(i, j);
+%         processed.C(:, :, :, i, j) = Y.C(i, j);
+%         processed.E(:, :, i, j) = Y.E(i, j);
+%         processed.S(:, :, i, j) = Y.S(i, j);
+%     end
+% end
+processed = Y;
 
-% Display analysis
+%% analysis
+% Prompt for importance balancing
+MetricPrompt = {'Relative importance of coral evenness for cultural ES (proportion):', ...
+        'Relative importance of structural complexity for cultural ES (proportion):', ...
+        'Relative importance of coral evenness for provisioning ES (proportion):', ...
+        'Relative importance of structural complexity for provisioning ES (proportion):', ...
+        'Total coral cover at which scope to support Cultural ES is maximised:', ...
+        'Total coral cover at which scope to support Provisioning ES is maximised:', ...
+        'Row used as counterfactual:'};
+dlgtitle = 'Coral metrics and scope for ecosystem-services provision';
+dims = [1, 50];
+definput = {'0.5', '0.5', '0.2', '0.8', '0.5', '0.5', '1'};
+answer = inputdlg(MetricPrompt, dlgtitle, dims, definput, "off");
+evcult = str2double(answer{1});
+strcult = str2double(answer{2});
+evprov = str2double(answer{3});
+strprov = str2double(answer{4});
+TCsatCult = str2double(answer{5});
+TCsatProv = str2double(answer{6});
+cf = str2double(answer{7}); %counterfactual
+
+ES_vars = [evcult, strcult, evprov, strprov, TCsatCult, TCsatProv, cf];
+
+ecosys_results = coralsToEcosysServices(processed, ES_vars);
 analyseADRIAresults1(ecosys_results);
-
-
-%% Save results to file
-
-% TODO: Extract RCP setting from `params` via ADRIAparms
-
-% Generate common file prefix
-filename = ADRIA_resultFilePrefix(params.RCP, alg_ind);
-
-% Save reef results
-ADRIA_saveResults(reef_condition_metrics, strcat(filename, '_reef'));
-
-% Save ecosystem results into separate file
-ADRIA_saveResults(ecosys_results, strcat(filename, '_ES'))
