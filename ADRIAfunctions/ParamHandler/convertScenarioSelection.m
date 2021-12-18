a64b26ed1a736ad4f4a2bd56adb992be20f1c7c0
ConnectedSystems <takuyai@gmail.com>
2021-12-18 17:07:53 +1100

    Address annoying warning messages due to dynamic table construction

--- ADRIAfunctions/ParamHandler/convertScenarioSelection.m
+++ ADRIAfunctions/ParamHandler/convertScenarioSelection.m
@@ -23,11 +23,9 @@ function converted = convertScenarioSelection(sel_values, p_opts)
 
     % For each selection, map the option id back to intended values
     % Note:
-    % This approach adds a new row every loop, which is slow but works.
-    % Columns may be of variable type/length and its a pain to 
+    % This approach adds a new column every loop, which is slow but works.
+    % Columns may be of variable types is a pain to 
     % write a clean approach using a pre-allocated table.
-    converted = table;
-    
     if ~istable(sel_values)
         if iscell(sel_values)
             sel_values = cell2mat(sel_values);
@@ -36,17 +34,18 @@ function converted = convertScenarioSelection(sel_values, p_opts)
         sel_values = array2table(sel_values, 'VariableNames', p_opts.name);
     end
     
+    converted = table;
     for p = 1:length(p_opts.name)
         pname = p_opts.name(p);
         ptype = p_opts.ptype(p);
         selection = sel_values.(pname);
-        % col(1:length(selection)) = {NaN};
-        % converted(:, p) = {1:length(selection)};
+
+        converted.(pname) = NaN(length(selection), 1);
         for sel = 1:length(selection)
             % convert from cell array to matrix if needed
             if ptype == "categorical" || ptype == "integer"
                 tmp = floor(selection(sel));
-                if tmp == p_opts.upper_bound{p} % && tmp == selection(sel)
+                if tmp == p_opts.upper_bound{p}
                     % subtract a small constant to ensure flooring works
                     % as intended when the value is at upper limit
                     tmp = max(floor(tmp - 1e-6), 1);
