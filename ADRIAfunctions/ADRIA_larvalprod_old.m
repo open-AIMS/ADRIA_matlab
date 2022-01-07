311c07b55257f6468fbd0b825c947489a5b0be99
KRNA01 <91235035+KRNA01@users.noreply.github.com>
2022-01-07 14:05:25 +1000

    Set up original larval prod function as old

---
+++ ADRIAfunctions/ADRIA_larvalprod_old.m
@@ -0,0 +1,21 @@
+function Y = ADRIA_larvalprod_old(tstep, assistadapt, natad, stresspast, LPdhwcoeff, DHWmaxtot, LPDprm2)
+% Project larval production for each coral type for the given time step.
+%
+% Inputs:
+%    tstep : int,
+%    assistadapt : array, DHW
+%    natad : array, DHWs per year for all species
+%    stresspast : array, DHW at previous time step for each site
+%    LPdhwcoeff : float,
+%    DHWmaxtot : int, maximum DHW
+%    LPDprm2 : int, larval production parameter 2?
+
+
+% Notes:  need to apply this only to coral groups
+
+ad = assistadapt + tstep .* natad;
+
+tmp_ad = (1 - ad / DHWmaxtot);
+Y = 1 - exp(-(exp(-LPdhwcoeff*(stresspast .* tmp_ad - LPDprm2))));
+
+end
