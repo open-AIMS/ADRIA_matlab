bb38e0fc3e1710d6949bb4551d09a258cdff6e76
ConnectedSystems <takuyai@gmail.com>
2022-01-10 14:09:57 +1100

    Slight expansion to comment for clarity

--- examples/running_ADRIA/programmatic_single_scenario.m
+++ examples/running_ADRIA/programmatic_single_scenario.m
@@ -9,7 +9,8 @@ ai = ADRIA();
 %% 2. Build a parameter table using default values
 
 % Resulting table consists of N rows and D columns, where N is the
-% number of scenarios (here, just one), and D is the number of parameters.
+% number of scenarios (here, a single simulation with default values), and 
+% D is the number of parameters.
 param_table = ai.raw_defaults;
 
 % See the "Parameter Interface" section in the documentation for
