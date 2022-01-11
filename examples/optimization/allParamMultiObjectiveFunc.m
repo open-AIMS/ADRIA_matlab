bdbe1606107b9dade14f621dc8b53cee6a01243a
Rosejoycrocker <56939532+Rosejoycrocker@users.noreply.github.com>
2022-01-11 10:34:33 +1000

    Changes to optimisation functions and example script
    - now uses new model class ADRIA()
    - currently only optimises w.r.t. Seed1,Seed2,SRM,Natad and Aatad. Allowing selection of variables to optimise w.r.t may be added later.
    - currently only optimises w.r.t TC (other metric calculations to be added.

--- examples/optimization/allParamMultiObjectiveFunc.m
+++ examples/optimization/allParamMultiObjectiveFunc.m
@@ -1,99 +1,49 @@
-function av_res = allParamMultiObjectiveFunc(x, alg, tgt_names, ...
-                                       combined_opts, nsites, ...
-                                       wave_scens, dhw_scens, ...
-                                       params, ecol_parms, ...
-                                       TP_data, site_ranks, ...
-                                       strongpred, ES_vars)
+function out_metrics = allParamMultiObjectiveFunc(x, ai, modified_params, Nreps, tgt_names)
     % Objective function that runs a single ADRIA simulation for all
-    % parameters.
+    % parameters, allowing variable parameter inputs for interventions.
     %
-    % Gives total coral cover TC as single output averaged over sites and time.
+    % Gives a vector of total average outputs in the order of tgt_names.
+    
     % Input : 
     %     x             : array, perturbed parameters
-    %     alg           : int, ranking algorithm 
+    %     ai           : ADRIA class object
     %     tgt_names      : cell of strs, name of output to optimize (TC, E, S, CES, PES)
-    %     combined_opts : table, ADRIA parameter details
-    %     nsites        : int, number of sites
-    %     wave_scens    : matrix, available wave damage scenarios
-    %     dhw_scens     : matrix, available DHW scenarios
-    %     params        : array, core ADRIA parameter values (TO BE REPLACED)
-    %     ecol_parms    : array, ADRIA ecological parameter values (TO BE REPLACED)
-    %     TP_data       : array, Transition probability data
-    %     site_ranks    : array, site centrality data
-    %     strongpred    : array, data indicating strongest predecessor per site
+    %     modified_params : table, ADRIA parameter details, as modified in
+    %                       multiObjOptimisation
     %
     % Output : 
-    %     av_res : average result (specified by tgt_name) over time/sites
+    %     out_metrics : average output metrics (specified by tgt_name) over
+    %                   time/sites/corals
 
     %% Convert sampled values back to ADRIA expected values
-    converted_tbl = convertScenarioSelection(x, combined_opts);
-    scenarios = table2array(converted_tbl);
+     modified_params(1,'Seed1' )= {(1)};
+     modified_params(1,'Seed2') = {x(2)};
+     modified_params(1,'SRM') = {x(3)};
+     modified_params(1,'Natad') = {x(4)};
+     modified_params(1,'Aadapt') = {x(5)};
+     
+     Y = ai.run(modified_params, sampled_values = false, nreps = Nreps);
+     
+     
+     out_metrics = zeros(Nreps,length(tgt_names));
+     for m = 1:length(tgt_names)
+         % total coral cover
+         if strcmp(tgt_names{m},'TC')
+             out_metrics(m)= mean(Y.all,'all');
+             %eveness
+         elseif strcmp(tgt_names{m},'E')
+             [~,E] = coralEvennessADRIA(Y);
+             out_metrics(m) = mean(E,'all');
+             %shelter volume
+         elseif strcmp(tgt_names{m},'SV')
+             SV = shelterVolumeADRIA(Y,ai.corals);
+             out_metrics(m) = mean(SV,'all');
+             % density of juvinile corals
+         elseif strcmp(tgt_names{m},'DJ')
+             DJ = Y.all(:,1:6:end,:)+ Y.all(:,2:6:end,:);
+             DJ = squeeze(sum(DJ,2));
+             out_metrics(m) = mean(DJ,'all');
+         end
+     end
 
-    %% Subset scenario spec for use
-    interv = scenarios(:, 1:9);
-    criteria_weights = scenarios(:, 10:end);  % need better way of separating values...
-
-    % How many wave/DHW scenarios to use
-    num_reps = 10;  % should move this to function definition...
-
-    %% Setup output
-    % Create temporary struct
-    tmp_s.TC = 0;
-    tmp_s.E = 0;
-    tmp_s.S = 0;
-
-    Y = repmat(tmp_s, 1, num_reps);
-
-    %% Run ADRIA
-    parfor i = 1:num_reps
-        w_scen = wave_scens(:, :, i);
-        d_scen = dhw_scens(:, :, i);
-        Y(i) = runADRIAScenario(interv, criteria_weights, ...
-                                params, ecol_parms, ...
-                                TP_data, site_ranks, strongpred, nsites, ...
-                                w_scen, d_scen, alg);
-    end
-
-    %% Process results
-    % check if any ES to be calculated
-    if any(strcmp(tgt_names,'CES'))||any(strcmp(tgt_names,'PES'))
-        tf = params.tf;
-        processed = struct('TC', zeros(tf, nsites, 1, num_reps), ...
-                       'E', zeros(tf, nsites, 1, num_reps), ...
-                       'S', zeros(tf, nsites, 1, num_reps),...
-                       'CES', zeros(tf, nsites, 1, num_reps),...
-                       'PES', zeros(tf, nsites, 1, num_reps));
-        for i = 1:num_reps
-            processed.TC(:, :, :, i) = Y(i).TC;
-            processed.E(:, :, :, i) = Y(i).E;
-            processed.S(:, :, :, i) = Y(i).S;
-        end
-
-        outs_ES = coralsToEcosysServices(processed,ES_vars);
-        
-        if any(strcmp(tgt_names,'CES'))
-            processed.CES = outs_ES.CES;
-        elseif any(strcmp(tgt_names,'PES'))
-            processed.PES = outs_ES.PES;
-        end
-
-    else
-        %% Process results
-        tf = params.tf;
-        processed = struct('TC', zeros(tf, nsites, 1, num_reps), ...
-                       'E', zeros(tf, nsites, 1, num_reps), ...
-                       'S', zeros(tf, nsites, 1, num_reps));
-        for nn = 1:length(tgt_names)
-            tgt_name = tgt_names{nn};
-            for i = 1:num_reps
-                processed.(tgt_name)(:, :, :, i) = Y(i).(tgt_name);
-            end
-        end
-    end
-
-    %% Average over sites/time
-    av_res = zeros(length(tgt_names));
-    for kk = 1:length(tgt_names)
-        av_res(kk) = mean(processed.(tgt_names{k}), 'all');
-    end
 end
--- examples/optimization/optimisation_example.m
+++ examples/optimization/optimisation_example.m
@@ -3,23 +3,19 @@
 
 % use simplest MDCA algorithm for now
 alg = 1;
-
 rcp = 60;
+Nreps = 50;
 
+filename = 'MooreTPMean.xlsx';
 % optimisation specification - want to optimise TC and CES
-names_vec = cell(2,1);
+names_vec = cell(1,1);
 names_vec{1} = 'TC';
-names_vec{2} = 'CES';
+%names_vec{2} = 'E';
 
-% load Moore reef data
-[TP_data, site_ranks, strongpred] = siteConnectivity('Inputs/MooreTPmean.xlsx', 0.1);
-fn = strcat("Inputs/example_wave_DHWs_RCP", num2str(rcp), ".nc");
+% perform optimisation 
+[x,fval] = multiObjOptimization(alg, rcp, Nreps, filename, names_vec);
 
-% perform optimisation (takes a while, be warned, improvements to
-% efficiency to be made)
-[x,fval] = multiObjOptimization(alg, names_vec, fn, TP_data, site_ranks, strongpred,rcp);
-
-% print results (also automatically saved to a struct in a .mat file) 
+% print results 
 sprintf('Optimal intervention values were found to be Seed1: %1.4f, Seed2: %1.4f, SRM: %2.0f, AsAdt: %2.0f, NatAdt: %1.2f, with av_TC = %1.4f',...
     x(1),x(2),x(3),x(4),x(5),fval);
 
