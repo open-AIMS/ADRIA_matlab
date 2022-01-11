02b7fa568769fd42802cecbbccfa288d7022060d
ConnectedSystems <takuyai@gmail.com>
2022-01-11 15:49:54 +1100

    Split indexing function

--- taxaIndex.m
+++
@@ -1,7 +0,0 @@
-function tInd = taxaIndex(taxa,taxa_id)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
-    ind_tmp =1:length(taxa_id);
-    tInd = ind_tmp(taxa_id == taxa);
-end
-
