5d714514af7de5562bf4ac2e1354af2613d7d561
Rosejoycrocker <56939532+Rosejoycrocker@users.noreply.github.com>
2022-01-12 15:08:01 +1030

    Intervention runs on HPC script
    - scripts to run a range of intervention parameters on the hpc
    - data saved for ease of creating data tables for BBNs

--- examples/running_ADRIA_HPC/startjob_ADRIA_multipar_rns.sh
+++ examples/running_ADRIA_HPC/startjob_ADRIA_multipar_rns.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
+Count = 0
+
 for RCP in 26 45 60 85
 do 
-	for PrSites in 1 2 3
+	for ((Seed1=0; Seed1<1000000; Seed1+200000))
 	do
-		for ((Seed1=0; Seed1<0.001; Seed1+0.0002))
+		for ((Seed2=0; Seed2<1000000; Seed2+200000))
 		do
-			for ((Seed2=0; Seed2<0.001; Seed2+0.0002))
+			for ((SRM=0; SRM<12; SRM+2))
 			do
-				for ((SRM=0; SRM<8; SRM++))
+				for ((Aadpt=0; Aadpt<12; Aadpt+2))
 				do
-					for ((Aadpt=0; Aadpt<8; Aadpt++))
+					for ((Natad=0; Natad<0.1; Natad+0.02))
 					do
-						for ((Natad=0; Natad<0.1; Natad+0.02))
-							export RCP
-							export PrSites
-							export Seed1
-							export Seed2
-							export SRM
-							export Aadpt
-							export Natad
-							sh /export/home/q-z/rcrocker/examples/runfile_ADRIA_optimisation.sh
+						export RCP
+						export Seed1
+						export Seed2
+						export SRM
+						export Aadpt
+						export Natad
+						((Count++))
+						sh /export/home/q-z/rcrocker/examples/runfile_ADRIA_multipar_rns.sh
 					done
 				done
 			done
