a55e2f88ffa00a91a5d350e8f2b747c4a0a65a1f
KRNA01 <91235035+KRNA01@users.noreply.github.com>
2022-01-21 20:25:47 +1000

    Corrected math in shelterVolume

--- ADRIAfunctions/Metrics/ReefConditionIndex.m
+++ ADRIAfunctions/Metrics/ReefConditionIndex.m
@@ -11,13 +11,13 @@ function Y = ReefConditionIndex(X, coralEvenness, shelterVolume, coralTaxaCover)
 
 %% Total coral cover
 covers = coralTaxaCover(X);
-TC = covers.covers.total_cover;
+TC = covers.total_cover;
 
 %% Coral evenness 
 E = coralEvenness(X);
 
 %% Shelter volume
-SV = shelterVolume(X);
+SV = shelterVolume(X, @coral_params);
 
 %% Coral juveniles
 juveniles = covers.juveniles; 
@@ -40,56 +40,47 @@ rci_crit = [...
     0.15    0.25    0.30    0.25;
     0.05    0.15    0.18    0.15];
 
+
 %% Adding dimension for rci metrics 
 
-A_TC = TC > rci_crit(1,1);
-B_TC = TC > rci_crit(2,1);
-C_TC = TC > rci_crit(3,1);
-D_TC = TC > rci_crit(4,1);
-E_TC = TC > rci_crit(5,1);
+criteria_threshold = 0.75; %threshold for how many criteria need to be met for category to be satisfied.
 
+ntsteps = size(X,1);
+nsites = size(X,2);
+ninterventions = size(X,3);
+nsims = size(X,4);
 
-A = sum(M,4)metrics) > rci_crit(1, metrics), 'omitnan') / numel(metrics);
-reefcondition = zeros(NREEFS, NYEARS);
+reefcondition = zeros(ntsteps, nsites, ninterventions, nsims);
 %Start loop for crieria vs metric comparisons
-for reef = 1:NREEFS
-    for t = 1:NYEARS
-        M = [rci.totalCover(reef, t), ...
-            rci.coralEvenness(reef, t), ...
-            rci.shelterVolume(reef, t), ...
-            rci.coraljuv_relative(reef, t), ...
-            rci.CCA(reef, t), ...
-            rci.COTSrel_complementary(reef, t), ...
-            rci.Macroalgae_complementary(reef, t), ...
-            rci.rubble_complementary(reef, t)];
-        A = sum(M(metrics) > rci_crit(1, metrics), 'omitnan') / numel(metrics);
-        B = sum(M(metrics) > rci_crit(2, metrics), 'omitnan') / numel(metrics);
-        C = sum(M(metrics) > rci_crit(3, metrics), 'omitnan') / numel(metrics);
-        D = sum(M(metrics) > rci_crit(4, metrics), 'omitnan') / numel(metrics);
-        E = sum(M(metrics) > rci_crit(5, metrics), 'omitnan') / numel(metrics);
+for tstep = 1:ntsteps
+    for site = 1:nsites
+        for int = 1:ninterventions
+            for sim = 1:nsims
+        M = [TC(tstep, site, int, sim), ...
+            E(tstep, site, int, sim), ...
+            SV(tstep, site, int, sim), ...
+            juveniles(tstep, site, int, sim)];
+        
+        A = sum(M > rci_crit(1, :), 'omitnan') / size(M,2);
+        B = sum(M > rci_crit(2, :), 'omitnan') / size(M,2);
+        C = sum(M > rci_crit(3, :), 'omitnan') / size(M,2);
+        D = sum(M > rci_crit(4, :), 'omitnan') / size(M,2);
+        E = sum(M > rci_crit(5, :), 'omitnan') / size(M,2);
     
-        if A > criteriaThreshold
-            reefcondition(reef, t) = 0.9; %representative of very good
-        elseif B > criteriaThreshold && A < criteriaThreshold
-            reefcondition(reef, t) = 0.7; %representative of good
-        elseif C > criteriaThreshold && A < criteriaThreshold && B < criteriaThreshold
-            reefcondition(reef, t) = 0.5; %representative of fair
-        elseif D > criteriaThreshold && C < criteriaThreshold && A < criteriaThreshold && B < criteriaThreshold
-            reefcondition(reef, t) = 0.3; %representative of poor
+        if A > criteria_threshold
+            reefcondition(tstep, site, int, sim) = 0.9; %representative of very good
+        elseif B > criteria_threshold && A < criteriaThreshold
+            reefcondition(tstep, site, int, sim) = 0.7; %representative of good
+        elseif C > criteria_threshold && A < criteriaThreshold && B < criteriaThreshold
+            reefcondition(tstep, site, int, sim) = 0.5; %representative of fair
+        elseif D > criteria_threshold && C < criteriaThreshold && A < criteriaThreshold && B < criteriaThreshold
+            reefcondition(tstep, site, int, sim) = 0.3; %representative of poor
         else
-            reefcondition(reef, t) = 0.1; %
-        end
-    end
-end
+            reefcondition(tstep, site, int, sim) = 0.1; %
+        end %if
+            end %sim
+        end %int
+    end %site
+end %tstep
 
-%% Plot results
-figure;
-tiledlayout(1, 4, 'TileSpacing', 'compact')
-for p = 1:4
-    ax(p) = nexttile;
-    colormap(flipud(turbo))
-    bubblechart(lons, lats, reefArea/1000, median(reefcondition(:, p*20-19:p*20), 2));
-    bubblesize([3, 30])
-    caxis([0.1, 0.9]);
-end
-colorbar('EastOutside')
+Y = reefcondition;
