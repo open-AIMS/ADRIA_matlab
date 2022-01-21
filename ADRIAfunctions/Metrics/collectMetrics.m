5b0107d8cf734746f105286b53f682e4a4d96c0d
ConnectedSystems <takuyai@gmail.com>
2022-01-21 11:38:41 +1100

    Tentative support for arbitrary anonymous functions

--- ADRIAfunctions/Metrics/collectMetrics.m
+++ ADRIAfunctions/Metrics/collectMetrics.m
@@ -10,6 +10,12 @@ function result_set = collectMetrics(Y, coral_params, metrics)
     for met = metrics
         func = met{1};
         func_name = func2str(func);
+        
+        if contains(func_name, "@")
+            tmp = regexp(func2str(func), ")", 'split', 'once');
+            func_name = regexprep(tmp{2}, {'[%()^,. ]+', '_+$'}, {'_', ''});
+        end
+
         result_set.(func_name) = func(Y, coral_params);
     end
 end
