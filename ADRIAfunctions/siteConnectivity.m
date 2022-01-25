663554115f854ca4d72212e5b39806304f9d2958
ConnectedSystems <takuyai@gmail.com>
2022-01-25 21:07:01 +1100

    Support loading & averaging multiple connectivity data sources together

--- ADRIAfunctions/siteConnectivity.m
+++ ADRIAfunctions/siteConnectivity.m
@@ -1,11 +1,13 @@
-function [TP_data, site_ranks, strongpred] =  siteConnectivity(file, con_cutoff)
+function [TP_data, site_ranks, strongpred] =  siteConnectivity(file_loc, con_cutoff, agg_func)
 % Create transitional probability matrix indicating connectivity between
 % sites, level of centrality, and the strongest predecessor for each site.
 %
 % Inputs:
-%   file       : str, path to data file to load
+%   file_loc   : str, path to data file (or datasets) to load.
+%                  If a folder, searches subfolders as well.
 %   con_cutoff : float, percent thresholds of max for weak connections in 
-%                  network (defined in ADRIAparms.m)
+%                  network (defined by user or defaults in simConstants)
+%   agg_func   : function_handle, defaults to `mean`.
 %
 % Output:
 %   TP_data    : table[float], containing the transition probability for 
@@ -15,14 +17,81 @@ function [TP_data, site_ranks, strongpred] =  siteConnectivity(file, con_cutoff)
 %
 % Example: 
 %     siteConnectivity('MooreTPmean.xlsx', 0.1)
-%% Load transitional probability matrix (connectivity between sites)
-F1 = readtable(file, 'PreserveVariableNames', true);
-F1(:,1:2) = [];  % remove the ID and address columns
-F1(1:2,:) = [];  % remove the ID and address rows
-TP1 = table2array(F1); % Transition probability matrix for all sites
-maxTP1cut = max(TP1,[],'all')*con_cutoff;
-TP1(TP1<maxTP1cut) = 0;  % filter out weak connections
-TPbase =  TP1;
+arguments
+    file_loc
+    con_cutoff {mustBeFloat}
+    agg_func = @mean
+end
+
+if isfolder(file_loc)
+    % Get list of files to combine
+    
+    % x(~[x.isdir])
+    files = dir(file_loc);
+    
+    t_dirs = [files.isdir];
+    if isempty(files(~t_dirs))
+        % no files in given directory, search subfolders
+        
+        t_file = strcat(file_loc, "/*/");
+        files = dir(t_file);
+        
+        t_dirs = [files.isdir];
+    end
+    
+    % Get list of files
+    t_files = files(~t_dirs);
+
+    locs = string({t_files.folder});
+    fns = string({t_files.name});
+    
+    % Create full file list
+    num_files = length(locs);
+    full_paths = string(zeros(num_files, 1));
+    for i = 1:length(locs)
+        full_paths(i) = fullfile(locs(i), fns(i));
+    end
+
+    % Load first file to determine size
+    x = readtable(full_paths(1), 'PreserveVariableNames', true, 'ReadVariableNames', true);
+    x(:, 1) = []; % remove row ID column
+    
+    [w, h] = size(x);
+    
+    data = zeros(length(full_paths), w, h);
+    data(1, :, :) = table2array(x);
+    for fn_i = 2:length(full_paths)
+        t_fn = full_paths(fn_i);
+        x = readtable(t_fn, 'PreserveVariableNames', true, 'ReadVariableNames', true);
+        x(:, 1) = [];  % remove row ID column
+        
+        % Store data
+        try
+            data(fn_i, :, :) = table2array(x); % Transition probability matrix for all sites
+        catch
+            x = 0;
+        end
+    end
+    
+    % aggregate data with indicated function
+    TPbase = squeeze(agg_func(data, 1));
+else
+    %% Load transitional probability matrix (connectivity between sites)
+    x = readtable(file_loc, 'PreserveVariableNames', true, 'ReadVariableNames', true);
+    
+    % When working with original data: "MooreTPMean.xlsx"
+    % F1(:,1:2) = [];  % remove the ID and address columns
+    % F1(1:2,:) = [];  % remove the ID and address rows
+
+    % remove site ids
+    % F1(1, :) = [];
+    x(:, 1) = [];
+    
+    TPbase = table2array(x); % Transition probability matrix for all sites
+end
+
+maxTP1cut = max(TPbase,[],'all')*con_cutoff;
+TPbase(TPbase<maxTP1cut) = 0;  % filter out weak connections
         
 % Change connectivity as a function of wind and tides
 TP_data(:,:,1) = TPbase;
