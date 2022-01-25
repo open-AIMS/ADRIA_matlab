585f4a550091f4cecf86021f8a75fca74b07ab79
ConnectedSystems <takuyai@gmail.com>
2022-01-25 21:09:51 +1100

    Initial update to setup function (incomplete but not sure if necessary)

--- ADRIAmain/setupADRIAsims.m
+++ ADRIAmain/setupADRIAsims.m
@@ -1,4 +1,4 @@
-function [wavedisttime, dhwdisttime] = setupADRIAsims(sims,params,nsites)
+function [wavedisttime, dhwdisttime] = setupADRIAsims(sims,params,nsites,DHW_fn, stochastic_fn)
     % Set up ADRIA simulations.
     % 
     % This setup script does one or two things.
@@ -19,41 +19,63 @@ function [wavedisttime, dhwdisttime] = setupADRIAsims(sims,params,nsites)
     % This latter analysis is handled in `analyseADRIA`.
 
     %% Simulate future wave exposure patterns from Puotinen and Callaghan SWH data
-    swhtbl = readtable('swhMoore_interp_ExpandedExample.xlsx', 'PreserveVariableNames', true); % import Marji's significant wave heights
-    
-    % col: SiteID, SiteAddress, Lon, Lat, Hs70, Hs80, Hs90, Hs95, Hs96, Hs97, Hs98, Hs99, Hs100
-    swh90 =  table2array(swhtbl(:,7)); % we use swhs within the 90 percentile  
-    swh100 = table2array(swhtbl(:,13));  % swhs within the 100 percentile (always) 
-    waveexp90 = swh90/(max(swh100)); % routine wave exposure rel to max 
-    mriskwaves = swh90/(7*max(swh90));  % placeholder conversion to mortality risks based on Madin paper
-    wavedisttime = 0.05*randn(params.tf,nsites,sims)+ waveexp90'; % projected spatial, 
-    % temporal and sims distribution of wave exposures assuming 5% variation ? very much a placeholder until we get real data.
-    
-    wavedisttime(wavedisttime>1) = 1; % constrain below unity
-    wavedisttime(wavedisttime<0) = 0; % constrain to positive values
+    if ~exist("DHW_fn", "var")
+        % Create dataset using older approach if source filename not specified
+        swhtbl = readtable('swhMoore_interp_ExpandedExample.xlsx', 'PreserveVariableNames', true); % import Marji's significant wave heights
+        
+        % col: SiteID, SiteAddress, Lon, Lat, Hs70, Hs80, Hs90, Hs95, Hs96, Hs97, Hs98, Hs99, Hs100
+        swh90 =  table2array(swhtbl(:,7)); % we use swhs within the 90 percentile  
+        swh100 = table2array(swhtbl(:,13));  % swhs within the 100 percentile (always) 
+        waveexp90 = swh90/(max(swh100)); % routine wave exposure rel to max 
+        mriskwaves = swh90/(7*max(swh90));  % placeholder conversion to mortality risks based on Madin paper
+        wavedisttime = 0.05*randn(params.tf,nsites,sims)+ waveexp90'; % projected spatial, 
+        % temporal and sims distribution of wave exposures assuming 5% variation ? very much a placeholder until we get real data.
 
-    %% Simulate future heat stress data based on Robson RECOM runs
-    % generate DHW time series
-    % Data is structure with resdhwsites, dhw_surf and z
-    F = load('MooreDHWs'); % load data that are previously generated
-    z = F.z; % bathymetry
-    
-    tmp_F = F.resdhwsites(:,5:7);
-    
-    [rows, ~] = size(tmp_F);
-    
-    if nsites ~= rows
-        dummy_F = repmat(tmp_F, ceil(nsites/rows), 1);
-        tmp_F = dummy_F(1:244, :);
+        wavedisttime(wavedisttime>1) = 1; % constrain below unity
+        wavedisttime(wavedisttime<0) = 0; % constrain to positive values
+
+        %% Simulate future heat stress data based on Robson RECOM runs
+        % generate DHW time series
+        % Data is structure with resdhwsites, dhw_surf and z
+        F = load('MooreDHWs'); % load data that are previously generated
+        z = F.z; % bathymetry
+
+        tmp_F = F.resdhwsites(:,5:7);
+
+        [rows, ~] = size(tmp_F);
+
+        if nsites ~= rows
+            dummy_F = repmat(tmp_F, ceil(nsites/rows), 1);
+            tmp_F = dummy_F(1:244, :);
+        end
+
+        mdhwdist0 = mean(tmp_F,2)'; %mean residual dhw at sites
+        sdhwdist0 =std(tmp_F,0,2)'; %standard deviation of residual dhws at sites
+        dhwdisttime = zeros(params.tf,nsites,sims); %initialise matrix that represents projections of DHW in space and time
+
+        for sim = 1:sims
+            dhwdisttime(:,:,sim) = ADRIA_DHWprojectfun(params.tf,mdhwdist0,...
+                sdhwdist0,params.dhwmax25,params.RCP,params.wb1,params.wb2);
+        end
+        dhwdisttime(dhwdisttime <=0) = 0;
+        dhwdisttime(dhwdisttime > params.DHWmaxtot) = params.DHWmaxtot;
+        
+        return
     end
     
-    mdhwdist0 = mean(tmp_F,2)'; %mean residual dhw at sites
-    sdhwdist0 =std(tmp_F,0,2)'; %standard deviation of residual dhws at sites
-    dhwdisttime = zeros(params.tf,nsites,sims); %initialise matrix that represents projections of DHW in space and time
+    tf = params.tf;
+    RCP = num2str(params.RCP);
     
-    for sim = 1:sims
-        dhwdisttime(:,:,sim) = ADRIA_DHWprojectfun(params.tf,mdhwdist0,...
-            sdhwdist0,params.dhwmax25,params.RCP,params.wb1,params.wb2);
-    end
-    dhwdisttime(dhwdisttime <=0) = 0;
-    dhwdisttime(dhwdisttime > params.DHWmaxtot) = params.DHWmaxtot;
+    % Insert period into RCP identifier
+    RCP_id = insertAfter(RCP, 1, ".");
+    
+    % Otherwise, create using newer datasets
+    dhw_tbl = readtable(DHW_fn, true); % import indicated data set
+    stochastic_dhw = readtable(stochastic_fn, true);  % import file with stochastic data
+    
+    target_RCP_rows = stochastic_dhw(stochastic_dhw.RCP == RCP_id, :);
+end
+    
+    
+    
+    
