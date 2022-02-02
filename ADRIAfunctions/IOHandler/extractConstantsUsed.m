0ebb9aacb275d4b4dc90505c126925f4cc5e7394
ConnectedSystems <takuyai@gmail.com>
2022-02-03 10:57:20 +1100

    Add docstring

--- ADRIAfunctions/IOHandler/extractConstantsUsed.m
+++ ADRIAfunctions/IOHandler/extractConstantsUsed.m
@@ -1,4 +1,11 @@
 function consts = extractConstantsUsed(fn)
+% Convenience function to extract the constants used for a batch run.
+%
+% Inputs:
+%     fn : str, path and filename of input record in netCDF format
+%
+% Outputs:
+%     consts : struct, of constants used
     consts = simConstants();
     fns = fieldnames(consts);
     for f = string(fns')
