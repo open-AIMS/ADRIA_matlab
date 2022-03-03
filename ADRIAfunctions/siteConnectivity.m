9e299970a4d9894e67f5bdd09cc01b3e9581de1b
ConnectedSystems <takuyai@gmail.com>
2022-03-03 15:28:09 +1100

    Allow explicit transposition of connectivity data
    And remove defunct version of site selection that crept back in.

--- ADRIAfunctions/siteConnectivity.m
+++ ADRIAfunctions/siteConnectivity.m
@@ -1,4 +1,4 @@
-function [TP_data, site_ranks, strongpred, site_ids] =  siteConnectivity(file_loc, con_cutoff, agg_func)
+function [TP_data, site_ranks, strongpred, site_ids] =  siteConnectivity(file_loc, con_cutoff, agg_func, swap)
 % Create transitional probability matrix indicating connectivity between
 % sites, level of centrality, and the strongest predecessor for each site.
 %
@@ -12,19 +12,22 @@ function [TP_data, site_ranks, strongpred, site_ids] =  siteConnectivity(file_lo
 %   con_cutoff : float, percent thresholds of max for weak connections in 
 %                  network (defined by user or defaults in simConstants)
 %   agg_func   : function_handle, defaults to `mean`.
+%   swap       : logical, whether to transpose data.
 %
 % Output:
 %   TP_data    : table[float], containing the transition probability for 
 %                  all sites
 %   site_ranks : table[float], centrality for each site
 %   strongpred : matrix[float], strongest predecessor for each site
+%   transpose  : logical, whether to transpose matrix or not.
 %
 % Example: 
 %     siteConnectivity('MooreTPmean.xlsx', 0.1)
 arguments
     file_loc
     con_cutoff {mustBeFloat}
-    agg_func = @mean
+    agg_func = function_handle @mean
+    swap logical = false
 end
 
 if isfolder(file_loc)
@@ -55,7 +58,7 @@ if isfolder(file_loc)
     end
 
     % Load first file to determine size (width/height)
-    x = readtable(full_paths(1), 'PreserveVariableNames', true, 'ReadVariableNames', true);
+    x = readtable(full_paths(1), 'PreserveVariableNames', true, 'ReadVariableNames', true, 'CommentStyle', '#');
     
     % Retrieve and sort by site ids
     % This is to ensure identical indexes when matching up with spatial 
@@ -65,15 +68,20 @@ if isfolder(file_loc)
     [site_ids, order_idx] = sort(site_ids);  % reordering
     x = x(order_idx, order_idx);  % enforce order
     
+    x_tmp = table2array(x);
+    if swap
+        x_tmp = transpose(x_tmp);
+    end
+    
     site_ids = string(site_ids);
 
-    [w, h] = size(x);
+    [w, h] = size(x_tmp);
     
     data = zeros(length(full_paths), w, h);
-    data(1, :, :) = transpose(table2array(x));
+    data(1, :, :) = x_tmp;
     for fn_i = 2:length(full_paths)
         t_fn = full_paths(fn_i);
-        x = readtable(t_fn, 'PreserveVariableNames', true, 'ReadVariableNames', true);
+        x = readtable(t_fn, 'PreserveVariableNames', true, 'ReadVariableNames', true, 'CommentStyle', '#');
         x(:, 1) = [];  % remove row ID column
         
         % Reorder rows/columns, assuming identical ID orders...
@@ -82,18 +90,18 @@ if isfolder(file_loc)
         % Store data
         % Transition probability matrix for all sites
         % Data set is flipped from what ADRIA expects, so transpose.
-        data(fn_i, :, :) = transpose(table2array(x));
+        x_tmp = table2array(x);
+        if swap
+            x_tmp = transpose(x_tmp);
+        end
+        data(fn_i, :, :) = x_tmp;
     end
     
     % aggregate data with indicated aggregation method (default: mean)
     TPbase = squeeze(agg_func(data, 1));
 else
-    %% Load transitional probability matrix (connectivity between sites)
-    x = readtable(file_loc, 'PreserveVariableNames', true, 'ReadVariableNames', true);
-    
-    % When working with original data: "MooreTPMean.xlsx"
-    % F1(:,1:2) = [];  % remove the ID and address columns
-    % F1(1:2,:) = [];  % remove the ID and address rows
+    %% Load a single transitional probability matrix
+    x = readtable(file_loc, 'PreserveVariableNames', true, 'ReadVariableNames', true, 'CommentStyle', '#');
 
     % remove site ids
     % F1(1, :) = [];
@@ -105,7 +113,11 @@ else
     [site_ids, order_idx] = sort(site_ids);
     x = x(order_idx, order_idx);
     
-    TPbase = transpose(table2array(x)); % Transition probability matrix for all sites
+    % Transition probability matrix for all sites
+    TPbase = table2array(x);
+    if swap
+        TPbase = transpose(TPbase);
+    end
 end
 
 maxTP1cut = max(TPbase,[],'all')*con_cutoff;
--- ADRIAmain/ADRIA.m
+++ ADRIAmain/ADRIA.m
@@ -249,8 +249,9 @@ classdef ADRIA < handle
             arguments
                obj
                fileset string
-               conargs.agg_func function_handle
+               conargs.agg_func function_handle = @mean
                conargs.cutoff {mustBeFloat} = NaN
+               conargs.swap = false
             end
 
             if isnan(conargs.cutoff)
@@ -259,7 +260,7 @@ classdef ADRIA < handle
                cutoff = conargs.cutoff;
             end
 
-            [tp, sr, sp, site_ids] = siteConnectivity(fileset, cutoff);
+            [tp, sr, sp, site_ids] = siteConnectivity(fileset, cutoff, conargs.agg_func, conargs.swap);
 
             obj.TP_data = tp;
             obj.site_ranks = sr;
@@ -396,35 +397,6 @@ classdef ADRIA < handle
                     wtpredecseed, 'wtpredecshade', wtpredecshade);
                 [~, ~, ~,~, rankings] = ADRIA_DMCDA(dMCDA_vars,alg,sslog,prefseedsites,prefshadesites,rankings);
                 store_rankings(l,:,:) = rankings;
-            end
-        end
-        function [site ranks] = siteSelection(obj,wts,tols,nreps,tstep)
-            arguments
-                obj
-                wts struct
-                tols struct
-                nreps {mustBeInteger}
-                tstep {mustBeInteger}
-            end
-            if isempty(obj.site_data)
-                error("Site data not loaded! Preload with `loadSiteData()`");
-            end
-            
-            if isempty(obj.TP_data)
-                error("Connectivity data not loaded! Preload with `loadConnectivity()`");
-            end
-            max_depth = tol.depth_min + tol.depth_offset;
-            depth_criteria = (obj.site_data.sitedepth > -max_depth) & (obj.site_data.sitedepth < -tol.depth_min);
-            depth_priority = site_data{depth_criteria, "recom_connectivity"};
-
-            nsiteint = obj.constants.nsiteint;
-            for l = 1:nreps
-                dhw_step = dhw_scen(tstep,:,l);
-                dMCDA_vars = struct('site_ids', depth_priority, 'nsiteint', nsiteint, 'prioritysites', [], ...
-                    'strongpred', strong_pred, 'centr', site_ranks.C1, 'damprob', damprob, 'heatstressprob', heatstressprob, ...
-                    'sumcover', sumcover,'maxcover', max_cover, 'risktol', risktol, 'wtconseed', wtconseed, 'wtconshade', wtconshade, ...
-                    'wtwaves', wtwaves, 'wtheat', wtheat, 'wthicover', wthicover, 'wtlocover', wtlocover, 'wtpredecseed', wtpredecseed,...
-                    'wtpredecshade', wtpredecshade);
             end
         end
 
