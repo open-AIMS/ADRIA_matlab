b61f02173a9417b63c64efa004033661007ac0ba
takuyai <takuyai@gmail.com>
2022-03-10 18:39:55 +1100

    Minor change to avoid repeated transpose of scaled_seed1/2

--- ADRIAfunctions/coralScenario.m
+++ ADRIAfunctions/coralScenario.m
@@ -314,27 +314,28 @@ function results = coralScenario(interv, criteria, coral_params, sim_params, ...
         prop_loss = Sbl .* squeeze(Sw_t(p_step, :, :));
         Yin1 = Y_pstep .* prop_loss;
 
-        if fogging > 0.0
+        if fogging > 0.0 && ~all(prefshadesites == 0)
             Yin1(:, prefshadesites) = Yin1(:, prefshadesites) .* (1.0 - fogging);
             Yfog(tstep, prefshadesites) = fogging;
         end
 
         if (tstep <= (seed_start_year + seedyears)) && ~all(prefseedsites == 0)
-            col_area_seed1 = coral_params.colony_area_cm2(s1_idx) / (10^4); % extract colony areas for sites selected and convert to m^2
-            col_area_seed2 = coral_params.colony_area_cm2(s2_idx) / (10^4); % extract colony areas for sites selected and convert to m^2
+            % extract colony areas for sites selected and convert to m^2
+            col_area_seed1 = coral_params.colony_area_cm2(s1_idx) / (10^4);
+            col_area_seed2 = coral_params.colony_area_cm2(s2_idx) / (10^4);
 
             site_area_seed = site_data.area(prefseedsites) .* (site_data.k(prefseedsites) / 100); % extract site area for sites selected and scale by available space for populations (k)
 
-            scaled_seed1 = ((seed1 / nsiteint) * col_area_seed1) ./ site_area_seed;
-            scaled_seed2 = ((seed2 / nsiteint) * col_area_seed2) ./ site_area_seed;
+            scaled_seed1 = (((seed1 / nsiteint) * col_area_seed1) ./ site_area_seed)';
+            scaled_seed2 = (((seed2 / nsiteint) * col_area_seed2) ./ site_area_seed)';
 
             % Seed each site with the value indicated with seed1/seed2
-            Yin1(s1_idx, prefseedsites) = Yin1(s1_idx, prefseedsites) + scaled_seed1'; % seed Enhanced Tabular Acropora
-            Yin1(s2_idx, prefseedsites) = Yin1(s2_idx, prefseedsites) + scaled_seed2'; % seed Enhanced Corymbose Acropora
+            Yin1(s1_idx, prefseedsites) = Yin1(s1_idx, prefseedsites) + scaled_seed1; % seed Enhanced Tabular Acropora
+            Yin1(s2_idx, prefseedsites) = Yin1(s2_idx, prefseedsites) + scaled_seed2; % seed Enhanced Corymbose Acropora
 
             % Log seed values/sites
-            Yseed(tstep, 1, prefseedsites) = scaled_seed1'; % log site as seeded with Enhanced Tabular Acropora
-            Yseed(tstep, 2, prefseedsites) = scaled_seed2'; % log site as seeded with Enhanced Corymbose Acropora
+            Yseed(tstep, 1, prefseedsites) = scaled_seed1; % log site as seeded with Enhanced Tabular Acropora
+            Yseed(tstep, 2, prefseedsites) = scaled_seed2; % log site as seeded with Enhanced Corymbose Acropora
 
         end
 
