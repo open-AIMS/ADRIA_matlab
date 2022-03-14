912e663ebaf6adebd7788e774aec8c444ca271ef
ConnectedSystems <takuyai@gmail.com>
2022-03-14 13:27:52 +1100

    Update approach to loop over and summarize variables
    Slower, but uses less memory than reading all data then summarizing.

--- ADRIAfunctions/IOHandler/gatherSummary.m
+++ ADRIAfunctions/IOHandler/gatherSummary.m
@@ -1,4 +1,4 @@
-function collated_mets = gatherSummary(file_loc, target_var, summarize)
+function collated_mets = gatherSummary(file_loc, opts)
 % Gather summary statistics of results saved across many NetCDF files.
 % Should only used with result sets created by running ADRIA with
 % `ai.runToDisk(___, summarize=true)`
@@ -8,6 +8,8 @@ function collated_mets = gatherSummary(file_loc, target_var, summarize)
 %                  e.g., "./some_folder/file_prefix"
 %   target_var : str, variable to collate (returns all results if not
 %                  specified)
+%   scenarios  : array, of indices or booleans indicating which scenarios
+%                  to keep. Defaults to all.
 %   summarize  : logical, to generate a single set of summary stats (true), or
 %                  to keep summary stats separate for each scenario (false; the default)
 %
@@ -15,62 +17,141 @@ function collated_mets = gatherSummary(file_loc, target_var, summarize)
 %    collated_mets : struct, of summary stats for each metric across time (tf) and space (nsites).
     arguments
         file_loc string
-        target_var string = "all"  % collate raw results if nothing specified.
-        summarize logical = false % whether or not to collapse everything along time, or to keep individual scenarios
+        opts.target_var string = "all"  % collate raw results if nothing specified.
+        opts.scenarios = []
+        opts.summarize logical = false % whether or not to collapse everything along time, or to keep individual scenarios
     end
+    
+    target_var = opts.target_var;
+    scenarios = opts.scenarios;
+    summarize = opts.summarize;
 
     file_prefix = fullfile(file_loc);
     pat = strcat(file_prefix, '_*]].nc');
     file_info = dir(pat);
     folder = file_info.folder;
 
-    % Get relevant input set
-    input_md_fi = dir(strcat(file_prefix, "*]]_inputs.nc"));
-    inputs = extractInputsUsed(strcat(input_md_fi.folder, "/", input_md_fi.name));
-    N = height(inputs);
-    clear inputs;
-
-    % Store results in a cell array of structs
     % ds = datastore(target_files, "FileExtensions", ".nc", "Type", "file", "ReadFcn", @readDistributed);
-    Y_collated = cell(N, 1);
-    for file = file_info'
-        fn = file.name;
-        full_path = fullfile(folder, fn);
+    wb = waitbar(0, 'Collecting summarized result set...');
+    
+    % Get list of variable names
+    file = file_info(1);
+    fn = file.name;
+    full_path = fullfile(folder, fn);
 
-        [Ytable, md] = readDistributed(full_path, target_var);
-        
-        b_start = md.record_start;
-        var_names = string(Ytable.Properties.VariableNames);
-        collated_res = {};
-        if (target_var == "all") && ~ismember(target_var, var_names)                
-            for i = b_start:md.record_end
-                Y_collated(i) = {table2struct(Ytable(i - b_start + 1, :))};
+    % Get initial dataset
+    [Ycollated, ~] = readDistributed(full_path, target_var);
+    var_names = string(fieldnames(Ycollated));
+    if contains("all", var_names)
+        var_names = "all";
+    end
+
+    num_vars = length(var_names);
+    num_vars_str = num2str(num_vars);
+    v_count = 0;
+
+    % Collate each variable across all files
+    % Although slower than looping over files first, this approach will
+    % use less memory
+    for vid = 1:num_vars
+        vn = var_names(vid);
+
+        v_count = v_count + 1;
+
+        waitbar(v_count/num_vars, wb, strcat("Reading ", num2str(v_count), " of ", num_vars_str, " metrics/logs"));
+        for file = file_info(2:end)'
+            fn = file.name;
+            full_path = fullfile(folder, fn);
+
+            [Ystruct, ~] = readDistributed(full_path, vn);
+
+            nd = ndims(Ycollated.(vn));
+            if nd < 5
+                nd_tmp = nd;
+            elseif nd == 5
+                nd_tmp = 4;
+            end
+
+            Ycollated.(vn) = cat(nd_tmp, Ycollated.(vn), Ystruct.(vn));
+        end
+
+        if contains(vn, ["__mean", "__std", "__median", "__std", "__min", "__max"])
+            tmp = split(vn, "__");
+            stat = tmp(2);
+            stat_func = str2func(stat);
+
+            if ~isempty(scenarios)
+                tmp = Ycollated.(vn);
+                Ycollated.(vn) = tmp(:, :, scenarios);
+                clear tmp;
+            end
+
+            if summarize
+                % Otherwise just combine results into one giant result set
+                % collated_mets.(ent).(stat) = concatMetrics(Y_collated, tmp_fn);
+                if (contains(vn, "__min") || contains(vn, "__max"))
+                    Ycollated.(vn) = squeeze(stat_func(Ycollated.(vn), [], 3));
+                elseif contains(vn, "__std")
+                    Ycollated.(vn) = squeeze(stat_func(Ycollated.(vn), 0, 3));
+                else
+                    % mean/median
+                    Ycollated.(vn) = squeeze(stat_func(Ycollated.(vn), 3));
+                end
+            end
+        else
+            % log entries
+            nd = ndims(Ycollated.(vn));
+            if nd == 5
+                % ndSparse arrays don't like being summarized across
+                % multiple dimensions so apply mean twice
+                % once across replicates, another across scenarios
+                Ycollated.(vn) = squeeze(mean(Ycollated.(vn), 5));
+
+                if ~isempty(scenarios)
+                    tmp = Ycollated.(vn);
+                    Ycollated.(vn) = tmp(:, :, :, scenarios);
+                    clear tmp;
+                end
+
+                if summarize
+                    Ycollated.(vn) = squeeze(mean(Ycollated.(vn), 4));
+                end
+            elseif nd < 5
+                if vn == "fog_log"
+                    % Handle special case for fog_log with ndims == 4
+                    Ycollated.(vn) = squeeze(mean(Ycollated.(vn), 3));
+                end
+
+                if ~isempty(scenarios)
+                    tmp = Ycollated.(vn);
+                    Ycollated.(vn) = tmp(:, :, scenarios);
+                    clear tmp;
+                end
+
+                if summarize
+                    Ycollated.(vn) = squeeze(mean(Ycollated.(vn), 3));
+                end
             end
         end
     end
+
+    waitbar(1,wb,'Finishing');
     
-    fnames = string(fieldnames(Y_collated{1}));
+    waitbar(1, wb, 'Collating...');
     
+    fnames = string(fieldnames(Ycollated));
+
     % Get name of metric entries (without the suffix indicating the stats)
     split_fnames = arrayfun(@(x) split(x, "__"), fnames, 'UniformOutput', false);
     unique_entries = unique(string(cellfun(@(x) string(strjoin(x(1:end-1), "__")), split_fnames, 'UniformOutput', false)));
     unique_entries = unique_entries(strlength(unique_entries) > 0);
-
-    % Include ADRIA logs that aren't summary stats
+    
     collated_mets = struct();
-    log_entries = fnames(~contains(fnames, ["mean", "std", "median", "std", "min", "max"]));
-    if ~isempty(log_entries)
-        for logs = log_entries'
-            % Get the average across all replicates
-            tmp = concatMetrics(Y_collated, logs);
-            collated_mets.(logs) = ndSparse(mean(tmp, ndims(tmp)));
-            clear tmp
-        end
-    end
-
     for ent = unique_entries'
         if ~contains(strcat(ent, "__", "mean"), fnames)
-            % skip non-summarized entries (e.g., site selection logs)
+            % add log entries
+            collated_mets.(ent) = Ycollated.(ent);
+            Ycollated.(ent) = [];
             continue
         end
 
@@ -78,25 +159,16 @@ function collated_mets = gatherSummary(file_loc, target_var, summarize)
         collated_mets.(ent) = struct();
 
         for stat = ["mean", "median", "std", "min", "max"]
-            stat_func = str2func(stat);
+            % stat_func = str2func(stat);
             tmp_fn = strcat(ent, "__", stat);
-            if summarize
-                if ismember(stat, ["min", "max"])
-                    collated_mets.(ent).(stat) = squeeze(stat_func(concatMetrics(Y_collated, tmp_fn), [], 3));
-                elseif stat == "std"
-                    collated_mets.(ent).(stat) = squeeze(stat_func(concatMetrics(Y_collated, tmp_fn), 0, 3));
-                else
-                    % mean/median
-                    collated_mets.(ent).(stat) = squeeze(stat_func(concatMetrics(Y_collated, tmp_fn), 3));
-                end
-                
-                continue
-            end
             
-            % Otherwise just combine results into one giant result set
-            collated_mets.(ent).(stat) = concatMetrics(Y_collated, tmp_fn);
+            % Combine results into one giant result set
+            collated_mets.(ent).(stat) = Ycollated.(tmp_fn);
+            Ycollated.(tmp_fn) = [];
         end
     end
+
+    close(wb);
 end
 
 
@@ -124,7 +196,7 @@ function [result, md] = readDistributed(filename, target_var)
     end
 
     nsims = md.n_sims;
-    result = table();
+    result = struct();
 
     % Open netcdf file once
     ncid = netcdf.open(filename, 'NC_NOWRITE');
@@ -158,107 +230,59 @@ function [result, md] = readDistributed(filename, target_var)
             clear tmp;
         end
         
+        netcdf.close(ncid);
+        
         return
     end
 
     % Handle cases where results were grouped
     vars = file_info.Groups.Variables;
-    nsims = md.n_sims;
 
     % Get group IDs
     group_ids = netcdf.inqGrps(ncid);
-    
-    if (target_var == "all") && ~contains(target_var, string({vars.Name}))
-        % "all" is not in list of variables, so actually get everything
-
-        % prep table first
-        for v = 1:length(vars)
-            result.(vars(v).Name) = cell(nsims, 1);
+    var_names = string({vars.Name});
+    if contains(target_var, var_names)
+        % Extract a single variable
+        result.(target_var) = [];
+        num_vars = 1;
+        var_names = var_names(var_names == target_var);
+    else
+        % If above two conditions are not matched, then extract everything
+        for v = 1:length(var_names)
+            result.(var_names(v)) = [];
         end
+        
+        num_vars = length(var_names);
+    end
 
-        % Collate data
-        num_groups = length(group_ids);
+    % Collate data
+    num_groups = length(group_ids);
+    log_match = ["_log", "_rankings"];
+    for v_id = 1:num_vars
+        v = var_names(v_id);
         for run_id = 1:num_groups
             grp_id = group_ids(run_id);
-            var_ids = netcdf.inqVarIDs(grp_id);
-    
-            for v_pos = 1:length(var_ids)
-                v_id = var_ids(v_pos);
-                v = netcdf.inqVar(grp_id, v_id);
+            var_id = netcdf.inqVarID(grp_id, v);
 
-                result(run_id, v) = {netcdf.getVar(grp_id, v_id, 'single')};
+            tmp = netcdf.getVar(grp_id, var_id, 'single');
+
+            nd = ndims(tmp);
+            if nd == 5
+                nd_tmp = 4;
+            else
+                nd_tmp = nd+1;
             end
+
+            if contains(v, log_match)
+                result.(v) = ndSparse(cat(nd_tmp, result.(v), tmp));
+            else
+                result.(v) = cat(nd_tmp, result.(v), tmp);
+            end
+
+            clear tmp;
         end
-
-%         for v = 1:length(vars)
-%             var_name = vars(v).Name;
-%             for run_id = 1:nsims
-%                 entry = strcat("/", "run_", num2str(run_id), "/", var_name);
-% 
-%                 var_id = netcdf.inqVarID(ncid, entry);
-%                 tmp = netcdf.getVar(ncid, var_id, 'single');
-%                 
-%                 switch ndims(tmp)
-%                     case 5
-%                         result(run_id, v) = {tmp(:, :, :, 1, :)};
-%                     case 4
-%                         result(run_id, v) = {tmp(:, :, 1, :)};
-%                     case {2,3}
-%                         result(run_id, v) = {tmp};
-%                     otherwise
-%                         error("Unexpected number of dims in result file");
-%                 end
-%             end
-%             
-%             tmp = [];  % clear tmp
-%         end
-    elseif contains(target_var, string({vars.Name}))
-        % Single target variable
-        result.(target_var) = cell(nsims, 1);
-
-        % Collate data
-        num_groups = length(group_ids);
-        for run_id = 1:num_groups
-            grp_id = group_ids(run_id);
-
-            var_id = netcdf.inqVarID(grp_id, target_var);
-            result(run_id, target_var) = {netcdf.getVar(grp_id, var_id, 'single')};
-
-%             switch ndims(tmp)
-%                 case 5
-%                     result(run_id, target_var) = {tmp(:, :, :, 1, :)};
-%                 case 4
-%                     result(run_id, target_var) = {tmp(:, :, 1, :)};
-%                 case {2,3}
-%                     result(run_id, target_var) = {tmp};
-%                 otherwise
-%                     error("Unexpected number of dims in result file");
-%             end
-% 
-%             tmp = [];
-        end
-
-%     elseif target_var == "all"
-%         % single variable named "all"
-%         result.(target_var) = repmat({0}, nsims, 1);
-%         for run_id = 1:nsims
-%             grp_name = strcat("run_", num2str(run_id));
-%             entry = strcat('/', grp_name, '/', 'all');
-%             
-%             var_id = netcdf.inqVarID(ncid, entry);
-%             tmp = netcdf.getVar(ncid, var_id, 'single');
-% 
-%             switch ndims(tmp)
-%                 case 5
-%                     result(run_id, 1) = {tmp(:, :, :, 1, :)};
-%                 case 4
-%                     result(run_id, 1) = {tmp(:, :, 1, :)};
-%                 otherwise
-%                     error("Unexpected number of dims in result file");
-%             end
-%         end
-%         
-%         tmp = [];
+        
+        result.(v) = squeeze(result.(v));
     end
     
     netcdf.close(ncid);
