ea8ff8cb54bb9f280d7e44c32204a98d998f3ea4
takuyai <takuyai@gmail.com>
2022-03-18 22:37:15 +1100

    Preallocate entire result set, and only collect averaged logs
    Necessary to avoid out-of-memory errors

--- ADRIAfunctions/IOHandler/gatherSummary.m
+++ ADRIAfunctions/IOHandler/gatherSummary.m
@@ -25,6 +25,10 @@ function collated_mets = gatherSummary(file_loc, opts)
     target_var = opts.target_var;
     scenarios = opts.scenarios;
     summarize = opts.summarize;
+    
+    % Get relevant input set
+    input_md_fi = dir(strcat(file_loc, "*]]_inputs.nc"));
+    N = height(extractInputsUsed(strcat(input_md_fi.folder, "/", input_md_fi.name)));
 
     file_prefix = fullfile(file_loc);
     pat = strcat(file_prefix, '_*]].nc');
@@ -40,56 +44,117 @@ function collated_mets = gatherSummary(file_loc, opts)
     full_path = fullfile(folder, fn);
 
     % Get initial dataset
-    [Ycollated, ~] = readDistributed(full_path, target_var, scenarios);
-    var_names = string(fieldnames(Ycollated));
-    if target_var == "all" && contains("all", var_names)
-        var_names = "all";
+    [Yfirst, ~] = readDistributed(full_path, target_var, scenarios);
+    var_names = string(fieldnames(Yfirst));
+    if length(target_var) == 1
+        if strcmp(target_var, "all") && any(contains(var_names, "all"))
+            var_names = "all";
+        end
+    else
+        % Collect the specified target variables
+        var_names = target_var;
+    end
+    
+    Ycollated = struct();
+    fnames = string(fieldnames(Yfirst));
+    for fn_id = 1:length(fnames)
+        fn = fnames(fn_id);
+        nd = ndims(Yfirst.(fn));
+        cat_dim = nd;
+        
+        sz = size(Yfirst.(fn));
+        
+        % Create dimension indexer, as we don't know how big each
+        % dimension is.
+        sel = repmat({1}, 1, min(nd, 4));
+        for i = 1:min(length(sz), 4)
+            sel(i) = {1:sz(i)};
+        end
+        
+        sz(cat_dim) = N;
+        Ycollated.(fn) = zeros(sz, 'single');
+        Ycollated.(fn)(sel{:}) = Yfirst.(fn);
     end
 
     num_vars = length(var_names);
     num_vars_str = num2str(num_vars);
+    num_files = length(file_info);
+    num_files_str = num2str(num_files);
     v_count = 0;
 
     % Collate each variable across all files
     % Although slower than looping over files first, this approach will
     % use less memory
     for vid = 1:num_vars
+        f_count = 1;  % first file is already read in
         vn = var_names(vid);
 
         v_count = v_count + 1;
         
-        try
-            % Wrap call to wait bar around try/catch in case user
-            % accidentally closes the dialog
-            waitbar(v_count/num_vars, wb, strcat("Reading ", num2str(v_count), " of ", num_vars_str, " metrics/logs"));
-        catch
-        end
+        nd = ndims(Ycollated.(vn));
+        concat_dim = nd;  % dimension to concat over
 
         for file = file_info(2:end)'
+            f_count = f_count + 1;
+
+            try
+                % Wrap call to wait bar around try/catch in case user
+                % accidentally closes the dialog
+                var_msg = strcat("Reading ", num2str(v_count), "/", num_vars_str, " metrics/logs, ");
+                file_msg = strcat("from file ", num2str(f_count), "/", num_files_str);
+                waitbar((v_count*f_count)/(num_vars*num_files), wb, strcat(var_msg, file_msg));
+            catch
+            end
+        
             fn = file.name;
             full_path = fullfile(folder, fn);
 
-            [Ystruct, ~] = readDistributed(full_path, vn, scenarios);
+            [Ystruct, md] = readDistributed(full_path, vn, scenarios);
 
             if isempty(Ystruct.(vn))
                 continue
             end
             
-            if isempty(Ycollated.(vn))
-                Ycollated.(vn) = Ystruct.(vn);
-                continue
+%             if v == "site_rankings"
+%                 sel = repmat({1}, 1, nd);
+%                 tmp_d = [md.n_sites, 2, num_groups];
+%                 nd = 3;
+%                 scen_dim = 3;
+%             elseif contains(v, "_log")
+%                 if v == "seed_log"
+%                     tmp_d = [md.n_timesteps, 2, md.n_sites, N];
+%                     nd = 4;
+%                     scen_dim = 4;
+%                 elseif v == "fog_log"
+%                     tmp_d = [md.n_timesteps, md.n_sites, num_groups];
+%                     nd = 3;
+%                     scen_dim = 3;
+%                 end
+%             end
+%             
+            sel = repmat({1}, 1, nd);
+            sz = size(Ystruct.(vn));
+            sz_len = length(size(Ystruct.(vn)));
+            for i = 1:sz_len
+                if (i ~= concat_dim)
+                    sel(i) = {1:sz(i)};
+                else
+                    sel(i) = {md.record_start:md.record_end};
+                end
             end
             
-            nd = ndims(Ycollated.(vn));
-            
-            switch nd
-                case {5, 4}
-                    nd_tmp = nd - 1;
-                case 3
-                    nd_tmp = nd;
+            if i == 5
+                sel = sel(1:4);
+            end
+
+            try
+                Ycollated.(vn)(sel{:}) = Ystruct.(vn);
+            catch ME
+                debug = 100;
             end
             
-            Ycollated.(vn) = cat(nd_tmp, Ycollated.(vn), Ystruct.(vn));
+            
+            clear Ystruct;
         end
 
         if contains(vn, ["__mean", "__std", "__median", "__std", "__min", "__max"])
@@ -103,35 +168,17 @@ function collated_mets = gatherSummary(file_loc, opts)
                 if (contains(vn, "__min") || contains(vn, "__max"))
                     Ycollated.(vn) = squeeze(stat_func(Ycollated.(vn), [], 3));
                 elseif contains(vn, "__std")
-                    Ycollated.(vn) = squeeze(stat_func(Ycollated.(vn), 0, 3));
+                    Ycollated.(vn) = squeeze(mean(Ycollated.(vn), 3));
                 else
                     % mean/median
                     Ycollated.(vn) = squeeze(stat_func(Ycollated.(vn), 3));
                 end
             end
         else
-            % log entries
-            nd = ndims(Ycollated.(vn));
-            if nd == 5
-                % ndSparse arrays don't like being summarized across
-                % multiple dimensions so apply mean twice
-                % once across replicates, another across scenarios
-                Ycollated.(vn) = squeeze(mean(Ycollated.(vn), 5));
-
-                if summarize
-                    Ycollated.(vn) = squeeze(mean(Ycollated.(vn), 4));
-                end
-            elseif nd == 4
-                % Handle special case for fog_log with ndims == 4
-                Ycollated.(vn) = squeeze(mean(Ycollated.(vn), 4));
-                
-                if summarize
-                    Ycollated.(vn) = squeeze(mean(Ycollated.(vn), 3));
-                end
-            else
-                if summarize
-                    Ycollated.(vn) = squeeze(mean(Ycollated.(vn), 3));
-                end
+            % Process log entries
+            if summarize
+                nd = ndims(Ycollated.(vn));
+                Ycollated.(vn) = squeeze(mean(Ycollated.(vn), nd));
             end
         end
     end
@@ -157,12 +204,12 @@ function collated_mets = gatherSummary(file_loc, opts)
             % add log entries
             collated_mets.(ent) = Ycollated.(ent);
             Ycollated.(ent) = [];
+
             continue
         end
 
         % Prep structure to hold results
         collated_mets.(ent) = struct();
-
         for stat = ["mean", "median", "std", "min", "max"]
             tmp_fn = strcat(ent, "__", stat);
             
@@ -188,25 +235,44 @@ function [result, md] = readDistributed(filename, target_var, scenarios)
     end
 
     % Get information about NetCDF data source
-    file_info = ncinfo(filename);
-    md = getADRIARunMetadata(filename);
+    % file_info = ncinfo(filename);
+    % md = getADRIARunMetadata(filename);
     
+    % Open netcdf file once
+    ncid = netcdf.open(filename, 'NC_NOWRITE');
+    [~, nvars, ~, ~] = netcdf.inq(ncid);
+    
+    md = getADRIARunMetadata(ncid);
+    
+    % Extract variable names and datatypes
+    is_group = false;
+    if nvars > 0
+        var_names = cell(nvars, 1);
+        for i = 1:nvars
+            [vn, ~, ~, ~] = netcdf.inqVar(ncid, i);
+            var_names{i} = vn;
+        end
+    else
+        is_group = true;
+        run_ids = netcdf.inqGrps(ncid);
+        var_ids = netcdf.inqVarIDs(run_ids(1));
+        var_names = cell(length(var_ids), 1);
+        for i = 1:length(var_ids)
+            [var_names{i}, ~, ~, ~] = netcdf.inqVar(run_ids(1), var_ids(i));
+        end
+    end
+
+    var_names = string(var_names);
+    if any(contains(var_names, target_var))
+        var_names = target_var;
+    end
+
     if ~isempty(scenarios)
         scenarios = ismember(md.record_start:md.record_end, scenarios);
         if ~any(scenarios)
+            % No matching scenarios in this file so return empty struct
             result = struct();
             
-            try
-                var_names = string({file_info.Variables.Name});
-            catch err
-                if err.message == "Dot indexing is not supported for variables of this type."
-                    vars = file_info.Groups.Variables;
-                    var_names = string({vars.Name});
-                else
-                    rethrow(err);
-                end
-            end
-
             if ismember(target_var, var_names)
                 result.(target_var) = [];
             else
@@ -219,28 +285,14 @@ function [result, md] = readDistributed(filename, target_var, scenarios)
         end
     end
 
-    % Extract variable names and datatypes
-    try
-        var_names = string({file_info.Variables.Name});
-        is_group = false;
-    catch ME
-        if strcmp("MATLAB:structRefFromNonStruct", ME.identifier)
-            is_group = true;
-        else
-            rethrow(ME)
-        end
-    end
-
-    nsims = md.n_sims;
     result = struct();
-
-    % Open netcdf file once
-    ncid = netcdf.open(filename, 'NC_NOWRITE');
 
     % TODO: Handle older ungrouped datasets
     if ~is_group
         % Handle older result structure where results were ungrouped
         n_vars = length(var_names);
+        
+        nsims = md.n_sims;
         for v = 1:n_vars
             var_n = var_names{v};
             if ~contains(target_var, var_n)
@@ -274,28 +326,18 @@ function [result, md] = readDistributed(filename, target_var, scenarios)
     end
 
     % Handle cases where results were grouped
-    vars = file_info.Groups.Variables;
 
     % Get group IDs
     group_ids = netcdf.inqGrps(ncid);
-    var_names = string({vars.Name});
-    if contains(target_var, var_names)
+    if contains(var_names, target_var)
         % Extract a single variable
-        result.(target_var) = [];
-        num_vars = 1;
-        var_names = var_names(var_names == target_var);
-    elseif target_var == "all"
-        % Extract everything
-        for v = 1:length(var_names)
-            result.(var_names(v)) = [];
-        end
-        
-        num_vars = length(var_names);
+        var_names = var_names(contains(var_names, target_var));
     end
+    
+    num_vars = length(var_names);
 
     % Collate data
     num_groups = length(group_ids);
-    % log_match = ["_log", "_rankings"];
     for v_id = 1:num_vars
         v = var_names(v_id);
 
@@ -308,36 +350,98 @@ function [result, md] = readDistributed(filename, target_var, scenarios)
 
             grp_id = group_ids(run_id);
             var_id = netcdf.inqVarID(grp_id, v);
-
+            
             tmp = netcdf.getVar(grp_id, var_id, 'single');
+            
+            if ~any(contains(fieldnames(result),v))
+                % Preallocate matrix
+                
+                % Get info on number of dimensions
+                if v == "site_rankings"
+                    tmp_d = [md.n_sites, 2, num_groups];
+                    nd = 3;
+                    scen_dim = 3;
+                elseif contains(v, "_log")
+                    if v == "seed_log"
+                        tmp_d = [md.n_timesteps, 2, md.n_sites, num_groups];
+                        nd = 4;
+                        scen_dim = 4;
+                    elseif v == "fog_log"
+                        tmp_d = [md.n_timesteps, md.n_sites, num_groups];
+                        nd = 3;
+                        scen_dim = 3;
+                    end
+                else
+                    [~, ~, dim_ids, ~] = netcdf.inqVar(grp_id, var_id);
+                    tmp_d = zeros(1, length(dim_ids));
+                    for i = 1:length(dim_ids)
+                        dims = dim_ids(i);
+                        [~, dl] = netcdf.inqDim(grp_id, dims);
+                        tmp_d(i) = dl;
+                    end
 
-            nd = ndims(tmp);
-            switch nd
-                case {5,4}
-                    nd_tmp = nd - 1;
-                case {2,3}
-                    nd_tmp = nd + 1;
+                    nd = ndims(tmp);
+                    
+                    % Preallocate matrix for results
+                    switch nd
+                        case {2,3}
+                            scen_dim = nd + 1;
+                            tmp_d = tmp_d(1:scen_dim);
+                        case {4,5}
+                            scen_dim = nd-1;
+                            tmp_d = tmp_d(1:4);
+                    end
+                    
+                    tmp_d(scen_dim) = num_groups;
+                end
+
+                result.(v) = zeros(tmp_d);
+                
+                % Create dimension indexer, as we don't know how big each
+                % dimension is.
+                if v ~= "site_rankings"
+                    sel = repmat({1}, 1, nd);
+                else
+                    sel = repmat({1}, 1, 3);
+                end
+                
+                for i = 1:length(tmp_d)
+                    sel(i) = {1:tmp_d(i)};
+                end
+            end 
+
+            sel{scen_dim} = run_id;
+            
+            if v == "site_rankings"
+                sel{2} = 1;
+                result.(v)(sel{:}) = siteRanking(tmp, "seed");
+                
+                sel{2} = 2;
+                result.(v)(sel{:}) = siteRanking(tmp, "shade");
+            else
+                if v == "seed_log"
+                    result.(v)(sel{:}) = mean(tmp, 5);
+                elseif v == "fog_log"
+                    result.(v)(sel{:}) = mean(tmp, 4);
+                else
+                    result.(v)(sel{:}) = tmp;
+                end
             end
-
-%             if contains(v, log_match)
-%                 result.(v) = ndSparse(cat(nd_tmp, result.(v), tmp));
-%             else
-%                 result.(v) = cat(nd_tmp, result.(v), tmp);
-%             end
-
-            result.(v) = cat(nd_tmp, result.(v), tmp);
 
             clear tmp;
         end
         
-        result.(v) = squeeze(result.(v));
+%         if v == "site_rankings"
+%             % Store as sparse matrix as site rankings are very large!
+%             result.(v) = ndSparse(result.(v));
+%         end
     end
     
     netcdf.close(ncid);
 end
 
 
-function md = getADRIARunMetadata(filename)
+function md = getADRIARunMetadata(ncid)
 % Helper function to collect metadata from netCDFs.
 % 
 % record_start
@@ -348,11 +452,13 @@ function md = getADRIARunMetadata(filename)
 % n_sites
 % n_species
 md = struct();
-md.record_start = ncreadatt(filename, "/", "record_start");
-md.record_end = ncreadatt(filename, "/", "record_end");
-md.n_sims = ncreadatt(filename, "/", "n_sims");
-md.n_reps = ncreadatt(filename, "/", "n_reps");
-md.n_timesteps = ncreadatt(filename, "/", "n_timesteps");
-md.n_sites = ncreadatt(filename, "/", "n_sites");
-md.n_species = ncreadatt(filename, "/", "n_species");
+
+ncdf_const = netcdf.getConstant('NC_GLOBAL');
+md.record_start = netcdf.getAtt(ncid, ncdf_const, 'record_start');
+md.record_end = netcdf.getAtt(ncid, ncdf_const, 'record_end');
+md.n_sims = netcdf.getAtt(ncid, ncdf_const, 'n_sims');
+md.n_reps = netcdf.getAtt(ncid, ncdf_const, 'n_reps');
+md.n_timesteps = netcdf.getAtt(ncid, ncdf_const, 'n_timesteps');
+md.n_sites = netcdf.getAtt(ncid, ncdf_const, 'n_sites');
+md.n_species = netcdf.getAtt(ncid, ncdf_const, 'n_species');
 end
