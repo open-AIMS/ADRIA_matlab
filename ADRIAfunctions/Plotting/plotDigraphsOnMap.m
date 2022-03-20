0afbd59a7d2e59f85c1ae77774139bf95c463202
KRNA01 <91235035+KRNA01@users.noreply.github.com>
2022-03-20 12:22:48 +1000

    Update plotDigraphsOnMap.m

--- ADRIAfunctions/Plotting/plotDigraphsOnMap.m
+++ ADRIAfunctions/Plotting/plotDigraphsOnMap.m
@@ -4,10 +4,17 @@
 ai = ADRIA();
 % Load site specific data
 % Must be loaded first
-ai.loadSiteData('.\Inputs/Brick/site_data/Brick_2015_637_reftable.csv');
+% Brick site data
+%ai.loadSiteData('Inputs/Brick/site_data/Brick_2015_637_reftable.csv');
+
+%Moore site data
+ai.loadSiteData('Inputs/Moore/site_data/MooreReefCluster_Spatial_w4.5covers.csv');
+
 
 % Path to folder or file. If folder, takes the average from all files in the given folder.
-ai.loadConnectivity('.\Inputs/Brick/connectivity/2015/');
+%ai.loadConnectivity('Inputs/Brick/connectivity/2016/');
+ai.loadConnectivity('Inputs/Moore/connectivity/2015/');
+
 
 % site data as used by ADRIA
 ai.site_data;
@@ -18,21 +25,37 @@ Y = ai.TP_data;
 lat = ai.site_data.lat;
 lon = ai.site_data.long;
 
-con_cutoff = 0.01;
+con_cutoff = 0.0001; %lower proportion of con
 maxY = max(Y,[],'all');
 maxYcut = maxY*con_cutoff;
 Y(Y<con_cutoff) = 0;  %filter out weak connections
+
+% Option: remove high outliers
+%Y(Y>200) = 0;
+
+%Option: scale with site area and k values
+Y = Y.* ai.site_data.area/1e4 .* ai.site_data.k/100 ;
+
+% Option: set self-seeding to zero (replace diagonal with zeros)
+%Y = Y - diag(diag(Y));
 
 DGbase = digraph(Y);
 EWbase = DGbase.Edges.Weight;
 C1 = centrality(DGbase,'outdegree','Importance',DGbase.Edges.Weight);
 
 %% Load reef map
-% first set
-lonmin = 149; 
-latmin = -20; 
-lonmax = 150; 
-latmax = -19; 
+% boundaries for Brick 
+% lonmin = 149.4; 
+% latmin = -19.85; 
+% lonmax = 149.85; 
+% latmax = -19.47; 
+
+% boundaries for Moore 
+lonmin = 146.12; 
+latmin = -16.96; 
+lonmax = 146.33; 
+latmax = -16.743; 
+
 bbox = [lonmin,latmin;lonmax,latmax];
 P = shaperead('Great_Barrier_Reef_Features.shp', 'BoundingBox', bbox);
 
@@ -45,7 +68,7 @@ hold on
 
 J1=plot(DGbase, 'XData', lon, 'YData', lat);
 J1.EdgeCData = nonzeros(EWbase);    % define edge colors
-J1.LineWidth = EWbase; %nonzeros(EWbase)*25;
+J1.LineWidth = nonzeros(EWbase)*2;
 J1.ArrowSize = 10; %nonzeros(EWbase)*300;
 J1.NodeFontSize = 10;
 J1.NodeColor = [0,0.3,0.3];
@@ -54,5 +77,5 @@ J1.MarkerSize = 2;
 set(gca,'FontSize', 14);
 set(gca,'color','none'); %set box to transparent
 colorbar;
-axis([149.48,149.9,-19.85,-19.45]); 
+axis([lonmin,lonmax,latmin,latmax]); 
 %end
