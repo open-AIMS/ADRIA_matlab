d9ce217669d3d1312f98f35f3fd1feaea4bcb25d
KRNA01 <91235035+KRNA01@users.noreply.github.com>
2022-04-09 15:34:35 +1000

    Add new plotting function: reef ranks

---
+++ ADRIAfunctions/Plotting/plotCoralGained.m
@@ -0,0 +1,15 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Plot trajectories of gains in coral real estate
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+ai = ADRIA();
+% Load site specific data
+% Must be loaded first
+% Brick site data
+ai.loadSiteData('C:\Users\KenAnthony\Documents\GitHub\ADRIA_repo\Inputs/Brick/site_data/Brick_2015_637_reftable.csv');
+
+ai.Load
+
+
+% site data as used by ADRIA
+ai.site_data;
--- ADRIAfunctions/Plotting/plotSiteRanksOnMap.m
+++ ADRIAfunctions/Plotting/plotSiteRanksOnMap.m
@@ -1,7 +1,9 @@
 %% Plot Brick reef site and reef ranks
 
 %% Load site ranks
-F = readtable('Brick_RCP45_seed_site_ranks.xlsx');
+F = readtable('Rankings_RCPs264560_connectivity2012_coralcover2025.xlsx');
+%F = readtable('Brick_RCP45_seed_site_ranks.xlsx');
+
 lat = F.lat;
 lon = F.long;
 rank = F.relative_rank;
